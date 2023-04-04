204a22e2d9afd8b2daa6df5ceefae49415dd87f4
czamora7 <czamora4@ewu.edu>
2023-04-04 16:22:39 -0700

    added vuetify

---
+++ wordle-web/src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <v-card>
+        <v-card-title>This is a vuetify class!</v-card-title>
+        <v-card-subtitle>(College Class)</v-card-subtitle>
+        
+        <v-card-text>TERHTYFYG JHFGDGJCVD JKHFGIYC VKHTDIYJ</v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
