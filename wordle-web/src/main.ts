47218e25dbdb8d6734f21711dc08d241a7f7053b
Ryan Golden <65426263+Xionxiii@users.noreply.github.com>
2023-04-04 16:37:17 -0700

    added vuetify icons and changed the look of the front v-card

--- wordle-web/src/main.ts
+++ wordle-web/src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { mdi } from 'vuetify/iconsets/mdi'
 
 import './assets/main.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 // Vuetify
 import 'vuetify/styles'
@@ -13,6 +15,12 @@ import * as directives from 'vuetify/directives'
 const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+        defaultSet: 'mdi',
+        sets: {
+            mdi
+        },
+    },
 })
 
 const app = createApp(App)
--- wordle-web/src/views/AboutView.vue
+++ wordle-web/src/views/AboutView.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <v-card>
+            <v-card-title> This is a vue/vuetify class! </v-card-title>
+            <v-card-subtitle> (College Class) </v-card-subtitle>
+
+            <v-card-text> 
+             Lorem ipsum solte dolor sit amet blah blah 
+             <v-icon icon="mdi:mdi-abacus" />
+            </v-card-text>
+
+            <v-card-actions>
+                <v-spacer />
+                <v-btn text color="warning">Cancel</v-btn>
+                <v-btn text color="primary">OK</v-btn>
+            </v-card-actions>
+        </v-card>
+    </div>
 </template>
 
 <style>
