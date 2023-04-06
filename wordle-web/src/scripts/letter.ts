12e0d9785729f3586fa12d19847d29622e7b3ed7
Ryan Golden <65426263+Xionxiii@users.noreply.github.com>
2023-04-06 15:58:25 -0700

    added letter and tests

---
+++ wordle-web/src/scripts/letter.ts
@@ -0,0 +1,30 @@
+// Class that represents a letter in the wordle word and status
+// of the letter in the wordle word
+export enum LetterStatus {
+    NotGuessed = 0,
+    Correct,
+    Misplaced,
+    Wrong
+}
+
+export class Letter {
+    letter: string | null = null;
+    status: LetterStatus = LetterStatus.NotGuessed;
+
+    constructor(letter: string) {
+        this.letter = letter;
+    }
+
+    get color() {
+        switch (this.status) {
+            case LetterStatus.Correct:
+                return 'green';
+            case LetterStatus.Misplaced:
+                return 'orange';
+            case LetterStatus.Wrong:
+                return 'red';
+            default:
+                return 'gray';
+        }
+    }
+}
