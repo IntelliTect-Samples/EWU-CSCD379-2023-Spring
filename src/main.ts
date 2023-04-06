6399a1d37da75ee2327044b889a3945c4f37e55e
Gunnar Knutson <gknutson1@ewu.edu>
2023-04-06 16:03:40 -0700

    Add Vuetify

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,19 @@ import router from './router'
 
 import './assets/main.css'
 
-const app = createApp(App)
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+const app = createApp(App).use(vuetify)
 
 app.use(router)
 
