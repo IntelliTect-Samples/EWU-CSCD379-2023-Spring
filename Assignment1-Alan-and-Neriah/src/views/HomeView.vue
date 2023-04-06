6bd0d92e29fff175df2fcd83a67e1aee2154ba65
Helaman99 <80353517+arose-13@users.noreply.github.com>
2023-04-06 12:58:44 -0700

    Added `game.vue`
    Added the game view, and also updated the router and homepage to link to the game page.

--- Assignment1-Alan-and-Neriah/src/views/HomeView.vue
+++ Assignment1-Alan-and-Neriah/src/views/HomeView.vue
@@ -5,5 +5,8 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <TheWelcome />
+    <hr>
+    <br>
+    <center><v-btn to = "/game"> Game </v-btn></center>
   </main>
 </template>
