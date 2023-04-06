925ae5d8febcd7c2c11b19e80c0ac131c201e051
Ian Kaiserman <ian.kaiserman@gmail.com>
2023-04-06 14:33:55 -0700

    Class Code 4-6

---
+++ wordle-web/src/main.ts
@@ -0,0 +1,30 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    sets: {
+        mdi
+    }
+  }
+})
+const app = createApp(App)
+
+app.use(vuetify)
+
+app.use(router)
+
+app.mount('#app')
---
+++ wordle-web/src/views/AboutView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <v-card>
+      <v-card-title> This is a Vuetify Card </v-card-title>
+      <v-card-subtitle> This is a subtitle </v-card-subtitle>
+      <v-card-text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est
+        labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam
+        neque porro libero rerum unde voluptatem!
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn text color="warning"> Cancel </v-btn>
+        <v-btn text color="primary"> OK </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
