814de13174d2d0bc46c8fbc6edf809f6a5fde550
Danielson77789 <59825907+Danielson77789@users.noreply.github.com>
2023-04-06 19:37:44 -0700

    Some work following along in class

---
+++ wordle-web/src/scripts/__tests__/letter.spec.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import { Letter, letterStatus } from '../letter'
+
+describe('Letter', () => {
+  it('Gets Correct Color', () => {
+    const letter = new Letter('a')
+    letter.status = letterStatus.Correct;
+    expect(letter.color).toBe('green');
+  })
+})
---
+++ wordle-web/src/scripts/letter.ts
@@ -0,0 +1,31 @@
+// Class that represents a letter in the wordle word and status
+
+export enum letterStatus {
+    NotGuessed = 0,
+    Correct,
+    Misplaced,
+    Wrong
+}
+
+export class Letter {
+    letter: string; 
+    status: letterStatus = letterStatus.NotGuessed;
+
+    constructor(letter: string) {
+        this.letter = letter;
+    }
+
+    get color() {
+        switch(this.status) {
+            case letterStatus.Correct:
+                return 'green';
+            case letterStatus.Misplaced:
+                return 'Yellow';
+            case letterStatus.Wrong:
+                return 'Red';
+            default: 
+                return 'Grey';    
+        }
+    }
+
+}
