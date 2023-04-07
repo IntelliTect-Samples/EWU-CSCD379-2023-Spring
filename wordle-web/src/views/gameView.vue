c4e003b35b5cf2a4c884f8cc682ed30545bc9408
Alex <alexalacasella@gmail.com>
2023-04-06 20:07:45 -0700

    Changed card and added placeholder text

--- wordle-web/src/views/gameView.vue
+++ wordle-web/src/views/gameView.vue
@@ -2,12 +2,19 @@
     <div class="game">
         <h1> This is the game page </h1>
     </div>
+    <v-card 
+    class="mx-auto"
+    max-width="344"
+    variant="outlined"
+    >
+      <v-card-title> Wordle </v-card-title>
+      <v-card-subtitle>stuff</v-card-subtitle>
 
-    <v-card
-        class="mx-auto"
-        max-width="344"
-        variant="outlined"
-        >
-        <v-card-title> Wordle </v-card-title>
-        </v-card>
+      <v-card-text> This is the placeholder text for the button context</v-card-text>
+
+      <v-card-actions>
+        <v-spacer />
+        <V-btn text color="primary">IT WORKED</V-btn>
+      </v-card-actions>
+    </v-card>
 </template>
