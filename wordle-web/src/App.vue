3605736861d83e93643545491c67e65dc50b0087
Nolan Posey <nposey1@ewu.edu>
2023-04-07 14:12:38 -0700

    Made game page and v-card for page.

--- wordle-web/src/App.vue
+++ wordle-web/src/App.vue
@@ -1,8 +1,3 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
@@ -13,12 +8,18 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/Game">Game</RouterLink>
       </nav>
     </div>
   </header>
 
   <RouterView />
 </template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
 
 <style scoped>
 header {
---
+++ wordle-web/src/views/Game.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="about">
+      <v-card
+        theme="dark"
+      >
+        <v-card-title> The Good Word </v-card-title>
+
+        <v-card-subtitle> The word guessing game! </v-card-subtitle>
+
+        <v-card-text> 
+            This is the game page. This text is being used as a placeholder as per the documentation requirements. 
+        </v-card-text>
+
+        <v-card-actions>  
+
+            <v-spacer />
+            
+            <v-btn
+                varient="tonal"
+                rounded="xl"
+                color="#5865f2"
+                elevation="24"
+            >
+             Home 
+            </v-btn>
+
+        </v-card-actions>
+
+      </v-card>
+    </div>
+</template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
