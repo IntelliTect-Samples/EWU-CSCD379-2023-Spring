443e76fbbb93cc3846d9fde2056729d21325d35c
Alex <alexalacasella@gmail.com>
2023-04-06 20:04:46 -0700

    Added card with title

--- wordle-web/src/views/gameView.vue
+++ wordle-web/src/views/gameView.vue
@@ -2,4 +2,12 @@
     <div class="game">
         <h1> This is the game page </h1>
     </div>
+
+    <v-card
+        class="mx-auto"
+        max-width="344"
+        variant="outlined"
+        >
+        <v-card-title> Wordle </v-card-title>
+        </v-card>
 </template>
