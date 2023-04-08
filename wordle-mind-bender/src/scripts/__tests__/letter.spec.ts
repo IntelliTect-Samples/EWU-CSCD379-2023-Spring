167507f178e65e667ba7344df7306d1b3e87a11e
AndrewKorchemniy <andrewkorch@gmail.com>
2023-04-08 11:02:47 -0700

    Letter Testing

---
+++ wordle-mind-bender/src/scripts/__tests__/letter.spec.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect } from 'vitest'
+import { Letter, LetterStatus } from '@/scripts/letter'
+
+describe('Letter', () => {
+    it('Gets Correct Color', () => {
+        const letter = new Letter('a')
+        letter.status = LetterStatus.Correct
+        expect(letter.color).toBe('green')
+    })
+
+    it('Gets Misplaced Color', () => {
+        const letter = new Letter('a')
+        letter.status = LetterStatus.Misplaced
+        expect(letter.color).toBe('orange')
+    })
+})
