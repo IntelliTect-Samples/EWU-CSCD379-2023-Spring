fd876b8c87fc98136cedb67b8e64373db6949607
Alex <alexalacasella@gmail.com>
2023-04-07 17:13:06 -0700

    Updated card placeholder text and letter test

--- wordle-web/src/views/gameView.vue
+++ wordle-web/src/views/gameView.vue
@@ -1,20 +1,16 @@
 <template>
     <div class="game">
         <h1> This is the game page </h1>
+
     <v-card 
     class="mx-auto"
-    max-width="344"
+    max-width="600"
+    max-height="400"
     variant="outlined"
     >
-      <v-card-title> Wordle </v-card-title>
-      <v-card-subtitle>stuff</v-card-subtitle>
-
-      <v-card-text> This is the placeholder text for the button context</v-card-text>
-
-      <v-card-actions>
-        <v-spacer />
-        <V-btn text color="primary">IT WORKED</V-btn>
-      </v-card-actions>
+      <v-card-title> Stirred Word </v-card-title>
+      <v-card-text>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. </v-card-text>
     </v-card>
+
     </div>
 </template>
