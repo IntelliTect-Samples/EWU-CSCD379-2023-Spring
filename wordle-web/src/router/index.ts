94979762869c2b100c0576a149677b7c6cd01248
Ryan Golden <65426263+Xionxiii@users.noreply.github.com>
2023-04-09 15:40:40 -0700

    fixed an error that happened when I renamed Game.vue to GameView.vue

--- wordle-web/src/router/index.ts
+++ wordle-web/src/router/index.ts
@@ -20,7 +20,7 @@ const router = createRouter({
     {
       path: '/game',
       name: 'game',
-      component: () => import('../views/Game.vue')
+      component: () => import('../views/GameView.vue')
     }
   ]
 })
