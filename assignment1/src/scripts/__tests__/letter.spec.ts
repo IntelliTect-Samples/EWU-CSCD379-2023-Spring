b3661a9796af810ddb3f947e7284bdad6fb88480
FenrisStargazer <80987915+FenrisStargazer@users.noreply.github.com>
2023-04-08 19:48:05 -0700

    Test again...

--- assignment1/src/scripts/__tests__/letter.spec.ts
+++ assignment1/src/scripts/__tests__/letter.spec.ts
@@ -1,27 +1,27 @@
 import { describe, it, expect } from 'vitest'
-import { Letter, LetterStatus } from '@/scripts/letter'
+import { letter, LetterStatus } from '@/scripts/letter'
 
 describe('Letter', () => {
     it('Gets Correct Color', () => {
-        const letter = new Letter('a')
+        const Letter = new letter('a')
         letter.status = LetterStatus.Correct
         expect(letter.color).toBe('green')
     })
 
     it('Gets Misplaced Color', () => {
-        const letter = new Letter('a')
+        const Letter = new letter('a')
         letter.status = LetterStatus.Misplaced
         expect(letter.color).toBe('gold')
     })
 
     it('Gets Wrong Color', () => {
-      const letter = new Letter('a')
+      const Letter = new letter('a')
       letter.status = LetterStatus.Wrong
       expect(letter.color).toBe('black')
   })
 
   it('Gets Unknown Color', () => {
-    const letter = new Letter('a')
+    const Letter = new letter('a')
     letter.status = LetterStatus.Unknown
     expect(letter.color).toBe('grey')
 })
