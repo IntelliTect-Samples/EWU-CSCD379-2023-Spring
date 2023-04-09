f2ecee72809ba622c3ae541a89538c6a4fa7bcb5
Tom Rohr <trohr92@live.com>
2023-04-09 16:12:11 -0700

    Added GameView and letter/unit tests from class.

---
+++ wordle-web/src/scripts/letter.ts
@@ -0,0 +1,28 @@
+//class that represents a letter in the wordle word and status of the letter
+export enum LetterStatus {
+    NotGuessed = 0,
+    Correct,
+    Misplaced,
+    Wrong
+}
+export class Letter {
+    letter: string;
+    status: LetterStatus = 0;
+
+    constructor(letter: string) {
+        this.letter = letter;
+    }
+
+    public getColor(): string {
+        switch (this.status) {
+            case LetterStatus.Correct:
+                return "green";
+            case LetterStatus.Misplaced:
+                return "yellow";
+            case LetterStatus.Wrong:
+                return "red";
+            default:
+                return "grey";
+        }
+    }
+}
