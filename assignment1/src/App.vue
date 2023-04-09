818be4925ffcb37cb6279f63ee3ac42cee265356
FenrisStargazer <80987915+FenrisStargazer@users.noreply.github.com>
2023-04-08 18:41:27 -0700

    Test?

--- assignment1/src/App.vue
+++ assignment1/src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="This is a thing!!" />
+      <HelloWorld msg="This is a thing ! !" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
