675b57730fa82c7102a096f727ca8c73da9d210d
DominicAlvarez <carlosdalvarez01@hotmail.com>
2023-04-09 13:24:23 -0700

    wordle-web commit

---
+++ wordle-web/src/main.ts
@@ -0,0 +1,25 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
+const vuetify = createVuetify({
+    components,
+    directives,
+
+})
+
+const app = createApp(App)
+
+app.use(vuetify)
+
+app.use(router)
+
+app.mount('#app')
