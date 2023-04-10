7dd3698aa3c5ea52bf9b12fa44fdc891f9b7a41f
daisypalencia <dpalencia@ewu.edu>
2023-04-09 22:24:38 -0700

    Adding letter tests

--- wordle-web/src/scripts/letter.ts
+++ wordle-web/src/scripts/letter.ts
@@ -1,6 +1,6 @@
 // Class that represents a letter in the wordle word and status
 // of the letter in the wordle word
-//push tests
+//push test
 export enum LetterStatus {
     NotGuessed = 0,
     Correct,
