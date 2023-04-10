c7347b246d03d50c08dba52557f76bc863df81d3
j0shh3ss <joshuahess11@gmail.com>
2023-04-09 18:26:44 -0700

    Moving to different branch

---
+++ vue-project/src/scripts/__tests__/letter.spec.ts
@@ -0,0 +1,11 @@
+//This is the testing file that tests letter.ts
+import { describe, it, expect } from 'vitest'
+import { Letter, LetterStatus } from '@/scripts/letter'
+
+describe('Letter', () => {
+    it('Gets Correct Color', () => {
+        const letter = new Letter('a')
+        letter.status = LetterStatus.Correct
+        expect(letter.color).toBe('green')
+    })
+})
