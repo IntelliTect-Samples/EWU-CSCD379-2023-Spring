0066a6db2d619c452b960171cb3d1044fa675ab0
Ben Knoll <bkbighorn@gmail.com>
2023-04-09 19:47:18 -0700

    Vue App Setup and game.vue
    - Setup Vue app
    - Added game.vue(button functionality doesn't work)

---
+++ wordle-webapp/src/views/AboutView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <v-card>
+      <v-card-title> This is a vue CLASS! </v-card-title>
+      <v-card-subtitle> (CLASS) </v-card-subtitle>
+
+      <v-card-text> 
+        Pizza 
+        <v-icon icon="mdi:mdi-abacus" />
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer />
+        <v-btn text>Cancel</v-btn>
+        <v-btn text>OK</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ wordle-webapp/src/views/game.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="game">
+      <v-card>
+        <v-card-title> Wordleopolis </v-card-title>
+        <v-card-subtitle> (CLASS) </v-card-subtitle>
+  
+        <v-card-text> 
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultrices sapien orci, nec varius massa tincidunt tincidunt. Suspendisse ultrices commodo ante vitae ullamcorper. Sed sed justo auctor, sodales lectus eu, volutpat odio.  
+          <v-icon icon="mdi:mdi-abacus" />
+        </v-card-text>
+  
+        <v-card-actions>
+          <v-spacer />
+          <v-btn text>Return Home</v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+
