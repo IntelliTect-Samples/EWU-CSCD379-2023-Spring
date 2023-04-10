857316d448c50beba12cea25f8e30066d1577e05
daisypalencia <dpalencia@ewu.edu>
2023-04-09 21:00:57 -0700

    changes made to AboutView and added mdi

--- wordle-web/src/App.vue
+++ wordle-web/src/App.vue
@@ -12,6 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">Class</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
--- wordle-web/src/main.ts
+++ wordle-web/src/main.ts
@@ -10,18 +10,18 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import { mdi } from  'vuetify/iconsets/mdi'
+import { mdi } from 'vuetify/iconsets/mdi'
 
 const vuetify = createVuetify({
-    components,
-    directives,
-    icons: {
-      defaultSet: 'mdi',
-      sets: {
-        mdi
-      }
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    sets: {
+      mdi
     }
-  })
+  }
+})
 
 const app = createApp(App)
 
