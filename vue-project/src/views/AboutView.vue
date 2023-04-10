c6f40f932b2f3965504c1329ecb1b26ed9cf4e5f
j0shh3ss <joshuahess11@gmail.com>
2023-04-09 19:06:27 -0700

    Adding game.vue

--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-card class="mx-auto" max-width="400">
+      <v-card-title>Word Scramble game</v-card-title>
+      <v-card-text>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt odio eget tellus euismod efficitur.</p>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn @click="$router.push('/')">Go back to Home</v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
