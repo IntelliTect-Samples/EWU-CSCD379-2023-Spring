10bd04a30a353e705bbdab15e8b5bfe13e22df06
grahamna4 <nate.a.graham@gmail.com>
2023-04-11 12:22:11 -0700

    assignment 1?

--- wordle-web/src/views/GameView.vue
+++ wordle-web/src/views/GameView.vue
@@ -1,14 +1,32 @@
+
+import { VSpacer } from 'vuetify/lib/components';
+
 <template>
-    <div class="game">
-      <v-card>
+  <div class="game">
+    <VCard variant="tonal">
+        <VCardTitle>
+            WordleCopy
+        </VCardTitle>
+          <v-divider :thickness="5"></v-divider>
         
-      </v-card>
-    </div>
+          <v-combobox
+          label="WordGuess"
+          clearable
+          :items="['should be dictionary']"
+        ></v-combobox>
+      <VCardActions>
+        <VSpacer></VSpacer>
+        <v-btn variant="tonal">
+          <RouterLink to="/">Home</RouterLink>
+        </v-btn>
+      </VCardActions>
+    </VCard>
+  </div>
   </template>
   
   <style>
     @media (min-width: 1024px) {
-      .about {
+      .game {
         min-height: 100vh;
         display: flex;
         align-items: center;
