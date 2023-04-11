fe4dc758ee642a46b0ef7eea32cdb6548087d36c
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2023-04-11 16:20:22 -0700

    Wordle letter validation

--- wordle-web/src/views/Wordle.vue
+++ wordle-web/src/views/Wordle.vue
@@ -15,6 +15,7 @@
 </template>
 
 <script setup lang="ts">
+import { Letter, LetterStatus } from '@/scripts/letter';
 import { ref } from 'vue';
 
 const guess = ref('')
@@ -31,11 +32,46 @@ function checkGuess() {
     return
   }
 
-  //check if the letters are valid
-  
-  //check if the lettes are in the right place
 
-  //
+  //check if the letters are valid
+  const results = new Array<Letter>()
+
+  //check if the letters are in the right place
+  const guessChars = guess.value.split('')
+  const secretChars = secretWord.split('')
+
+  let isCoorect = true
+  for(let i = 0; i < 5; i++) {
+    results.push(new Letter(guess.value[i]))
+    if(guess.value[i] === secretWord[i]) {
+      results[i].status = LetterStatus.Correct
+      guessChars[i] = '_'
+      secretChars[i] = '_'
+      console.log(`Letter ${i} is correct!`)
+    } else {
+      isCoorect = false
+      //results[i].status = LetterStatus.Misplaced
+      console.log(`Letter ${i} is incorrect!`)
+    }
+  }
+
+for(let i = 0; i < 5; i++) {
+  if (guessChars[i] === '_') {
+    continue
+  }
+  for(let j = 0; j < 5; j++) {
+    if (secretChars[j] === guessChars[i]) {
+      results[i].status = LetterStatus.Misplaced
+      guessChars[i] = '_'
+      secretChars[j] = '_'
+      console.log(`Letter ${i} is misplaced!`)
+      break
+    }
+  }
+}
+
+  console.log(guessChars)
+  console.log(secretChars)
 }
 
 //initialize the secret word
