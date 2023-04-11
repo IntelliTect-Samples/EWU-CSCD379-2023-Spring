5bf76dbc246ef0dbbc3e9aae8493a57780754795
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2023-04-11 15:19:31 -0700

    more Dills code

--- wordle-web/src/views/Wordle.vue
+++ wordle-web/src/views/Wordle.vue
@@ -1,13 +1,22 @@
 <template>
   <div >
       {{ count }}
-      <h1>Worlde Game</h1>
-      <v-text-field label="Label"></v-text-field>
+      <h1>Wordle Game</h1>
+      <v-text-field v-model="guess" label="Label"></v-text-field>
+
+      <h2> 
+        {{  guess }}
+      </h2>
+
+      <v-btn @click="checkGuess">Check</v-btn>
+
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+function checkGuess() {console.log(guess.value)}
+const guess = ref('')
 const count = ref(1)
 setInterval(() => { count.value++}, 1000 )
 </script>
