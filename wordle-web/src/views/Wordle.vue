d1ee721a2dcac79ab66d0f527e25c32c54d31e95
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2023-04-11 15:03:03 -0700

    testing Dills code

---
+++ wordle-web/src/views/Wordle.vue
@@ -0,0 +1,13 @@
+<template>
+  <div >
+      {{ count }}
+      <h1>Worlde Game</h1>
+      <v-text-field label="Label"></v-text-field>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const count = ref(1)
+setInterval(() => { count.value++}, 1000 )
+</script>
