ccb9c841ecda86cdb7e59b20e19c92c6492597bf
grahamna4 <nate.a.graham@gmail.com>
2023-04-11 13:48:56 -0700

    more tests for letter

--- wordle-web/src/scripts/__tests__/letter.spec.ts
+++ wordle-web/src/scripts/__tests__/letter.spec.ts
@@ -2,6 +2,12 @@ import { describe, it, expect } from 'vitest'
 import { Letter, LetterStatus } from '@/scripts/letter'
 
 describe('Letter', () => {
+    it('Gets correct String Val', () => {
+        const letter = new Letter('a')
+        letter.status = LetterStatus.Correct
+        expect(letter.letter).toBe('a')
+    })
+
     it('Gets Correct Color', () => {
         const letter = new Letter('a')
         letter.status = LetterStatus.Correct
@@ -12,5 +18,16 @@ describe('Letter', () => {
         const letter = new Letter('a')
         letter.status = LetterStatus.Misplaced
         expect(letter.color).toBe('orange')
+    })
+
+    it('Gets correct Wrong Color', () => {
+        const letter = new Letter('a')
+        letter.status = LetterStatus.Wrong
+        expect(letter.color).toBe('red')
+    })
+
+    it('Gets correct not Guessed Color', () => {
+        const letter = new Letter('a')
+        expect(letter.color).toBe('grey')
     })
 })
