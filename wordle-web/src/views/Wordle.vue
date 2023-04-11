d6ce325f8414e831eb9464ea10178beb5586c821
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2023-04-11 15:29:55 -0700

    It works!!!

--- wordle-web/src/views/Wordle.vue
+++ wordle-web/src/views/Wordle.vue
@@ -1,22 +1,22 @@
 <template>
   <div >
-      {{ count }}
       <h1>Wordle Game</h1>
       <v-text-field v-model="guess" label="Label"></v-text-field>
+
+      <v-btn @click="checkGuess">Check</v-btn>
 
       <h2> 
         {{  guess }}
       </h2>
 
-      <v-btn @click="checkGuess">Check</v-btn>
 
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-function checkGuess() {console.log(guess.value)}
+
 const guess = ref('')
-const count = ref(1)
-setInterval(() => { count.value++}, 1000 )
+function checkGuess() {console.log(guess.value)}
+
 </script>
