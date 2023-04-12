6763f2fce38a821ad4a7cfd8bc2fa21361da551e
Hunter T <strangeranger01@randomserver.xyz>
2023-04-12 15:00:12 -0700

    Rename view vue file

--- wordle-web/src/router/index.ts
+++ wordle-web/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Wordle from '../views/Wordle.vue'
+import Wordle from '../views/WordleView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
