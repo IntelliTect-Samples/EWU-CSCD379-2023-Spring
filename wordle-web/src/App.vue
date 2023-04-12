d9942cd42cce7b095065c38f308430817fdb46fd
grahamna4 <nate.a.graham@gmail.com>
2023-04-12 12:00:28 -0700

    ewu logo

--- wordle-web/src/App.vue
+++ wordle-web/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/ewuLogo.svg" width="150" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="Assignment 1 CSCD 379" />
