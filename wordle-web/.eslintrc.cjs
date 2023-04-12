3341d33e132a778f03eb5138243a7c9fde54af5a
Jason Stanley <jstanley3@ewu.edu>
2023-04-12 11:33:10 -0700

    Finished Assignment

--- wordle-web/.eslintrc.cjs
+++ wordle-web/.eslintrc.cjs
@@ -9,7 +9,13 @@ module.exports = {
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  rules: {
+    'vue/multi-word-component-names': ['error', {
+      'ignores': ['default']
+    }]
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
 }
+
