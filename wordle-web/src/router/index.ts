27a8ddb959099d870c17c017e6b425fa6d721c2b
grahamna4 <nate.a.graham@gmail.com>
2023-04-12 14:16:38 -0700

    changed file name to game.vue

--- wordle-web/src/router/index.ts
+++ wordle-web/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Wordle from '../views/Wordle.vue'
-import GameView from '../views/GameView.vue'
+import GameView from '../views/game.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
