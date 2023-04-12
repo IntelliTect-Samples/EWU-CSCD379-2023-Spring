20b9c4fa54d61f38265c9c734d26771b53f456cf
Nolan Posey <nposey1@ewu.edu>
2023-04-12 12:03:17 -0700

    Added more letter tests

--- wordle-web/src/scripts/tests/letter.spec.ts
+++ wordle-web/src/scripts/tests/letter.spec.ts
@@ -8,4 +8,22 @@ describe('Letter', () => {
     letter.status = LetterStatus.Correct
     expect(letter.Color).toBe('green')
   })
+
+  it('Gets NotGuessed Color', () => {
+    const letter = new Letter('a')
+    letter.status = LetterStatus.NotGuessed
+    expect(letter.Color).toBe('grey')
+  })
+
+  it('Gets Misplaced Color', () => {
+    const letter = new Letter('a')
+    letter.status = LetterStatus.Misplaced
+    expect(letter.Color).toBe('orange')
+  })
+
+  it('Gets Wrong Color', () => {
+    const letter = new Letter('a')
+    letter.status = LetterStatus.Wrong
+    expect(letter.Color).toBe('red')
+  })
 })
