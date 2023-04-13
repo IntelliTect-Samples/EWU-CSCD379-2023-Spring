08524f06d440c1baf3a8490136b7196a7392bc6b
Nolan Posey <nposey1@ewu.edu>
2023-04-12 19:07:18 -0700

    Github button works successfully

--- wordle-web/src/views/HomeView.vue
+++ wordle-web/src/views/HomeView.vue
@@ -14,7 +14,7 @@
 
       <v-btn varient="outlined" rounded="xl" color="#5865f2" elevation="24" to="/about"> About </v-btn>
 
-      <v-btn varient="outlined" rounded="xl" color="#5865f2" elevation="24" to="https://github.com/Nolan-Posey/EWU-CSCD379-2023-Spring/blob/main/Assignment1.md"> Github </v-btn>
+      <v-btn varient="outlined" rounded="xl" color="#5865f2" elevation="24" href="https://github.com/Nolan-Posey/EWU-CSCD379-2023-Spring/blob/main/Assignment1.md"> Github </v-btn>
     </v-card-actions>
   </v-card>
 </template>
