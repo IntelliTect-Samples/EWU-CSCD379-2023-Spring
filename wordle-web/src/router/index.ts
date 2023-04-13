84f9a39e9fa1a2e33c37c1a1c5d4ab13d3a09426
Alex <alexalacasella@gmail.com>
2023-04-13 14:25:27 -0700

    Renamed gameView.vue to GameView.vue and updated accordingly

--- wordle-web/src/router/index.ts
+++ wordle-web/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import GameView from '../views/GameView.vue'
+import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,16 +14,12 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: AboutView
     },
     {
       path: '/game',
       name: 'game',
-
-      component: () => import('../views/gameView.vue')
+      component: GameView
     }
   ]
 })
