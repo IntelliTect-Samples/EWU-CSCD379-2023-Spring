b6f8ce6e051781eed33f31a627a14c41d44e0041
AndrewKorchemniy <andrewkorch@gmail.com>
2023-04-13 09:33:11 -0700

    Implement grid layout and navbar

--- wordle-mind-bender/src/main.ts
+++ wordle-mind-bender/src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
--- wordle-mind-bender/src/scripts/__tests__/letter.spec.ts
+++ wordle-mind-bender/src/scripts/__tests__/letter.spec.ts
@@ -2,21 +2,21 @@ import { describe, it, expect } from 'vitest'
 import { Letter, LetterStatus } from '@/scripts/letter'
 
 describe('Letter', () => {
-    it('Gets Correct Color', () => {
-        const letter = new Letter('a')
-        letter.status = LetterStatus.Correct
-        expect(letter.color).toBe('green')
-    })
+  it('Gets Correct Color', () => {
+    const letter = new Letter('a')
+    letter.status = LetterStatus.Correct
+    expect(letter.color).toBe('green')
+  })
 
-    it('Gets Misplaced Color', () => {
-        const letter = new Letter('a')
-        letter.status = LetterStatus.Misplaced
-        expect(letter.color).toBe('orange')
-    })
+  it('Gets Misplaced Color', () => {
+    const letter = new Letter('a')
+    letter.status = LetterStatus.Misplaced
+    expect(letter.color).toBe('orange')
+  })
 
-    it('Gets Wrong Color', () => {
-        const letter = new Letter('a')
-        letter.status = LetterStatus.Wrong
-        expect(letter.color).toBe('red')
-    })
+  it('Gets Wrong Color', () => {
+    const letter = new Letter('a')
+    letter.status = LetterStatus.Wrong
+    expect(letter.color).toBe('red')
+  })
 })
