8f04475c254e2b105c1e45f53ba9781cb0b70c83
Cayden Kappes <caydenkappes@gmail.com>
2023-04-13 23:09:43 -0700

    Put about page into a v-card

--- wordle-clone/src/views/AboutView.vue
+++ wordle-clone/src/views/AboutView.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-card>
+        <v-card-title>
+            About Page Example
+        </v-card-title>
+        <v-card-item>
+            <div class="text-caption"> Just an about page, not much about it. </div>
+        </v-card-item>
+        <v-card-actions>
+            <v-btn variant="outlined">
+                <RouterLink to="/">Home</RouterLink>
+            </v-btn>
+            <v-btn variant="outlined">
+                <RouterLink to="/game">Game</RouterLink>
+            </v-btn>
+        </v-card-actions>
+    </v-card>
 </template>
 
 <style>
