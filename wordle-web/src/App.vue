43dea0507702e18449ea1346474c527fbc87591f
czamora7 <czamora4@ewu.edu>
2023-04-13 17:54:30 -0700

    added game.vue and a button to navigate to it from the home page

--- wordle-web/src/App.vue
+++ wordle-web/src/App.vue
@@ -11,8 +11,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+          <RouterLink to="/game">Game</RouterLink>
       </nav>
     </div>
   </header>
