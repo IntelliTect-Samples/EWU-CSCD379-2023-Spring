5cccce2b0bc49bc9e2b07147a6498e937230956d
Nick Parkman <nparkman@ewu.edu>
2023-04-13 20:51:53 -0700

    keypad test

--- wordle-web/src/views/Wordle.vue
+++ wordle-web/src/views/Wordle.vue
@@ -14,7 +14,34 @@
   </div>
 
   <h2>{{ guess }}</h2>
-  
+  <div>
+    <v-btn :color="letter.color">A</v-btn>
+    <v-btn :color="letter.color">B</v-btn>
+    <v-btn :color="letter.color">C</v-btn>
+    <v-btn :color="letter.color">D</v-btn>
+    <v-btn :color="letter.color">E</v-btn>
+    <v-btn :color="letter.color">F</v-btn>
+    <v-btn :color="letter.color">G</v-btn>
+    <v-btn :color="letter.color">H</v-btn> 
+    <v-btn :color="letter.color">I</v-btn> 
+    <v-btn :color="letter.color">J</v-btn> 
+    <v-btn :color="letter.color">K</v-btn> 
+    <v-btn :color="letter.color">L</v-btn> 
+    <v-btn :color="letter.color">M</v-btn> 
+    <v-btn :color="letter.color">N</v-btn> 
+    <v-btn :color="letter.color">O</v-btn> 
+    <v-btn :color="letter.color">P</v-btn> 
+    <v-btn :color="letter.color">Q</v-btn> 
+    <v-btn :color="letter.color">R</v-btn> 
+    <v-btn :color="letter.color">S</v-btn> 
+    <v-btn :color="letter.color">T</v-btn> 
+    <v-btn :color="letter.color">U</v-btn> 
+    <v-btn :color="letter.color">V</v-btn> 
+    <v-btn :color="letter.color">W</v-btn> 
+    <v-btn :color="letter.color">X</v-btn> 
+    <v-btn :color="letter.color">Y</v-btn> 
+    <v-btn :color="letter.color">Z</v-btn>      
+  </div>
 </template>
 <script setup lang="ts">
 import { WordleGame } from '@/scripts/wordleGame'
