97a0f320724f083053b7586ca538d477ad720a9a
Nick Parkman <nparkman@ewu.edu>
2023-04-13 19:33:28 -0700

    update code

--- wordle-web/src/scripts/letter.ts
+++ wordle-web/src/scripts/letter.ts
@@ -1,31 +1,30 @@
 // Class that represents a letter in the wordle word and status
 // of the letter in the wordle word
 export enum LetterStatus {
-    NotGuessed = 0,
-    Correct,
-    Misplaced,
-    Wrong
+  NotGuessed = 0,
+  Correct,
+  Misplaced,
+  Wrong
 }
 
-
 export class Letter {
-    letter: string;
-    status: LetterStatus = LetterStatus.NotGuessed;
+  char: string
+  status: LetterStatus = LetterStatus.NotGuessed
 
-    constructor(letter: string) {
-        this.letter = letter;
-    }
+  constructor(char: string) {
+    this.char = char
+  }
 
-    get color() {
-        switch (this.status) {
-            case LetterStatus.Correct:
-                return 'green';
-            case LetterStatus.Misplaced:
-                return 'orange';
-            case LetterStatus.Wrong:
-                return 'red';
-            default:
-                return 'grey';
-        }
+  get color() {
+    switch (this.status) {
+      case LetterStatus.Correct:
+        return 'green'
+      case LetterStatus.Misplaced:
+        return 'orange'
+      case LetterStatus.Wrong:
+        return 'red'
+      default:
+        return 'grey'
     }
+  }
 }
