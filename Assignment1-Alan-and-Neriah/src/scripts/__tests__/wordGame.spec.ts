c366e53a94a5ce635db2e216210f421d8ef9f401
schemcharger <ngraham7@ewu.edu>
2023-04-18 13:49:54 -0700

    Added Letter, Word, Game, and wordlist

---
+++ Assignment1-Alan-and-Neriah/src/scripts/__tests__/wordGame.spec.ts
@@ -0,0 +1,16 @@
+import {describe, it, expect} from 'vitest';
+import {WordGame} from '@/scripts/wordGame';
+
+describe('WordGame', () => {
+    it('submits a guess', () => {
+        const wordGame = new WordGame('word');
+        wordGame.submitGuess('word');
+        expect(wordGame.guesses.length).toBe(1);
+    });
+    it('restarts the game', () => {
+        const wordGame = new WordGame('word');
+        wordGame.submitGuess('word');
+        wordGame.restartGame('word');
+        expect(wordGame.guesses.length).toBe(0);
+    });
+});
---
+++ Assignment1-Alan-and-Neriah/src/scripts/letter.ts
@@ -0,0 +1,28 @@
+export enum LetterStatus {
+    correct,
+    incorrect,
+    notGuessed,
+    misplaced
+}
+
+export class Letter {
+    public letter: string;
+    public status: LetterStatus = LetterStatus.notGuessed;
+
+    constructor(letter: string) {
+        this.letter = letter
+    }
+
+    get color() {
+        switch (this.status) {
+            case LetterStatus.correct:
+                return "green"
+            case LetterStatus.incorrect:
+                return "red"
+            case LetterStatus.misplaced:
+                return "yellow"
+            default:
+                return "grey"
+        }
+    }
+}
