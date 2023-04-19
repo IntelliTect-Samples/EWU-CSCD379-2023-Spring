4d495ca1a8656b2c2e44320ab96a03d3f1a77630
DominicAlvarez <carlosdalvarez01@hotmail.com>
2023-04-18 17:08:17 -0700

    letter tests

---
+++ wordle-web/src/scripts/__tests__/letter.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+
+import { Letter, LetterStatus } from '@/scripts/letter'
+
+describe('Letter', () => {
+  it('Gets Correct Color', () => {
+    const letter = new Letter('a')
+    letter.status = LetterStatus.Correct
+    expect(letter).toBe('green')
+  })
+})
