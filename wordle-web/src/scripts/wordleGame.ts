ab5337556c8e7eefc94fab3b7b042f526ffc1f9c
Meghan <Meg.Woodford@IntelliTect.com>
2023-04-20 14:03:38 -0700

    Adding missing lecture code

--- wordle-web/src/scripts/wordleGame.ts
+++ wordle-web/src/scripts/wordleGame.ts
@@ -2,11 +2,15 @@ import { Word } from '@/scripts/word'
 import { WordsService } from './wordsService'
 
 export class WordleGame {
-  constructor(secretWord?: string | null) {
+  constructor(secretWord?: string, numberOfGuesses: number = 6) {
+    if (!secretWord) secretWord = WordsService.getRandomWord()
+    this.numberOfGuesses = numberOfGuesses
     this.restartGame(secretWord)
   }
   guesses = new Array<Word>()
   secretWord = ''
+  numberOfGuesses = 6
+  guess!: Word
 
   // // check length of guess
   //   if (this.letters.length !== secretWord.length) {
@@ -14,14 +18,25 @@ export class WordleGame {
   //     return
   //   }
 
-  restartGame(secretWord?: string | null) {
+  restartGame(secretWord: string) {
     this.secretWord = secretWord || WordsService.getRandomWord()
     this.guesses.splice(0)
+    // create a word for each guess
+    for (let iWord = 0; iWord < this.numberOfGuesses; iWord++) {
+      const word = new Word(secretWord.length)
+      this.guesses.push(word)
+    }
+    this.guess = this.guesses[0]
   }
 
-  submitGuess(guess: string) {
-    const word = new Word(guess)
-    this.guesses.push(word)
-    word.check(this.secretWord)
+  submitGuess() {
+    // put logic to win here.
+    this.guess.check(this.secretWord)
+    const index = this.guesses.indexOf(this.guess)
+    if (index < this.guesses.length - 1) {
+      this.guess = this.guesses[index + 1]
+    } else {
+      // The game is over
+    }
   }
 }
