6c16c84bc090576a502a8fa696ce32a16798b8e8
Grant Erickson <grant@granterickson.com>
2023-04-21 07:24:49 -0700

    Fixed tests

--- wordle-web/src/scripts/__tests__/letter.spec.ts
+++ wordle-web/src/scripts/__tests__/letter.spec.ts
@@ -5,12 +5,12 @@ describe('Letter', () => {
   it('Gets Correct Color', () => {
     const letter = new Letter('a')
     letter.status = LetterStatus.Correct
-    expect(letter.color).toBe('green')
+    expect(letter.color).toBe('correct')
   })
 
   it('Gets Misplaced Color', () => {
     const letter = new Letter('a')
     letter.status = LetterStatus.Misplaced
-    expect(letter.color).toBe('orange')
+    expect(letter.color).toBe('misplaced')
   })
 })
