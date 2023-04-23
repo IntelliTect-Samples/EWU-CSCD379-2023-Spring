d7952ac9ff6cbe9f203bffbd7dc6ea7b294225c1
Nolan Posey <nposey1@ewu.edu>
2023-04-22 18:27:57 -0700

    Added custom app bar component

--- wordle-web/src/App.vue
+++ wordle-web/src/App.vue
@@ -1,47 +1,25 @@
 <template>
   <v-app>
-    <header>
-      <!-- -- Old header before the implementation of the 'v-app-bar`. -- -->
-      <!--    <v-toolbar id="toolbar">-->
-      <!--      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="50" height="50" />-->
-      <!--      <v-toolbar-title>Wordle Web</v-toolbar-title>-->
-      <!--      <v-spacer></v-spacer>-->
-      <!--      <v-toolbar-items>-->
-      <!--        <v-btn @click="toggleTheme" :key="mode = theme.global.name.value" text>Toggle {{ mode }} mode</v-btn>-->
-      <!--      </v-toolbar-items>-->
-      <!--      <v-spacer></v-spacer>-->
-      <!--      <v-toolbar-items>-->
-      <!--        <v-btn to="/">Home</v-btn>-->
-      <!--        <v-btn to="/wordle">Wordle</v-btn>-->
-      <!--        <v-btn to="/about">About</v-btn>-->
-      <!--      </v-toolbar-items>-->
-      <!--    </v-toolbar>-->
-      <!--    <v-app-bar app color="primary" dark>-->
+    <AppBar></AppBar>
+    <main class="pt-16">
+      <v-spacer></v-spacer>
 
-      <v-app-bar id="toolbar" dark>
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="50" height="50" />
-        <v-toolbar-title>Wordle Web</v-toolbar-title>
-        <!-- TODO: Size of icon needs to be modified. -->
-        <v-icon @click="toggleTheme">mdi-lightbulb</v-icon>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      </v-app-bar>
-    </header>
+      <RouterView />
 
-    <!-- TODO: Maybe choose something better than '<br />' -->
-    <br />
-    <br />
-
-    <!-- TODO: Main text needs to be centered. -->
-    <RouterView />
+    </main>
   </v-app>
+  
+  
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import { useTheme } from 'vuetify/lib/framework.mjs'
+import AppBar from './components/AppBar.vue';
 
 const theme = useTheme()
 
+// TODO: Need to get toggle theme buttons inside of AppBar component
 function toggleTheme() {
   if (theme.global.name.value === 'light') {
     theme.global.name.value = 'dark'
---
+++ wordle-web/src/components/AppBar.vue
@@ -0,0 +1,65 @@
+<template>
+    <v-app-bar >
+
+      <img alt="books logo" class="logo" src="@/assets/books.png" width="50" height="50" color="white" />
+
+      <v-app-bar-title>The Good Word</v-app-bar-title>
+
+      <v-btn icon="mdi-arrow-right-bold-hexagon-outline" @click.stop="settings = !settings"></v-btn>
+
+      <v-app-bar-nav-icon @click.stop="menu = !menu"></v-app-bar-nav-icon>
+
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="settings" location="left" app>
+      <v-list>
+          <v-list-item title="Settings"></v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+      
+    </v-navigation-drawer>
+
+    <v-navigation-drawer v-model="menu" location="right" app>
+      <v-list>
+          <v-list-item title="Menu"></v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact">
+          <v-list-item>
+              <v-btn to="/">Home</v-btn>
+          </v-list-item>
+              
+          <v-list-item>
+              <v-btn to="/about">About</v-btn>
+          </v-list-item>
+
+          <v-list-item>
+              <v-btn to="/wordle">Play Game</v-btn>
+          </v-list-item>
+      </v-list>
+      
+    </v-navigation-drawer>
+    
+</template>
+
+
+<script lang="ts">
+
+  export default {
+      data: () => ({
+          menu: false,
+          settings: false,
+      }),
+
+      watch: {
+          group () {
+              this.menu = false
+              this.settings = false
+          }
+      }
+  }
+  
+</script>
