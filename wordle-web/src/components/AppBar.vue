b79cd6d51dbead01bdd55165707c97260b8d771b
TannerStephenson <117472530+TannerStephenson@users.noreply.github.com>
2023-04-22 18:25:38 -0700

    Clean up

--- wordle-web/src/components/AppBar.vue
+++ wordle-web/src/components/AppBar.vue
@@ -3,20 +3,26 @@
       class="mx-auto"
       max-width="448">
       <v-layout>
+        
         <v-app-bar
           density="compact">
           <template v-slot:prepend>
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-btn icon= "mdi-menu" @click.stop="hamburgerDrawer = !hamburgerDrawer">
+            </v-btn>
           </template>
           <v-icon icon="mdi-head-cog" size="x-large"></v-icon>
           <v-app-bar-title>Wordle</v-app-bar-title>
           <template v-slot:append>
-            <v-btn icon= "mdi-cog" @click.stop="drawer = !drawer">
+            <v-btn icon= "mdi-cog" @click.stop="settingDrawer = !settingDrawer">
             </v-btn>
           </template>
         </v-app-bar>
 
-        <v-navigation-drawer v-model="drawer" location="right">
+      <v-navigation-drawer temporary v-model="hamburgerDrawer" location="left">
+        
+      </v-navigation-drawer>
+
+      <v-navigation-drawer v-model="settingDrawer" location="right">
         <v-list>
           <v-btn @click="setInverseTheme"> Inverse Theme </v-btn>
           <v-btn @click="setDarkTheme"> Dark Theme </v-btn>
@@ -42,6 +48,6 @@ function setInverseTheme() {
 function setDarkTheme() {
   theme.global.name.value = 'dark'
 }
-
-const drawer = ref(false);
+const hamburgerDrawer = ref(false);
+const settingDrawer = ref(false);
 </script>
