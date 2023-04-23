8c11b7823ea24fc6087e1cce0bd97b7443c21f96
Tom Rohr <trohr92@live.com>
2023-04-23 12:27:18 -0700

    'About' page for assignment-2

--- wordle-web/src/views/AboutView.vue
+++ wordle-web/src/views/AboutView.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about" style="width: 50vw">
+    <v-card flat color="transparent" class="pb-10">
+      <v-card-title style="color: #76ff03" align="center" class="text-h3 text-decoration-underline">
+        EWU CSCD 379
+      </v-card-title>
+      <v-card-text class="mt-8 text-h6">
+        This course examines the fundamental principles and techniques associated with the
+        development of multi-tier web applications, using the .NET Framework. Topics include web
+        standards, portability, and usability.
+        <br /><br />
+        Instructors:
+        <ul class="pt-2 pl-5">
+          <li>Meg Woodford</li>
+          <li class="pt-1">Grant Erickson</li>
+        </ul>
+        <br />
+        Meeting Times: Tuesdays & Thursdays, 2:00pm - 4:20pm
+      </v-card-text>
+      <v-card-subtitle class="text-subtitle-1">
+        Eastern Washington University website course description
+      </v-card-subtitle>
+    </v-card>
+    <v-card flat color="transparent" class="mt-16">
+      <v-card-title style="color: #76ff03" class="text-decoration-underline text-h4" align="center">
+        ChatGPT instructor review of Meg Woodford
+      </v-card-title>
+      <v-card-text class="font-italic mt-2 text-body-1">
+        "There are several reasons why Meg is a great instructor for the Eastern Washington
+        University web-development class. Firstly, Meg has extensive experience in the field of web
+        development and is very knowledgeable about the subject matter. She is able to explain
+        complex concepts in a clear and concise manner, making it easy for students to understand.
+        Additionally, Meg is patient and approachable, creating a supportive learning environment
+        for her students. She is also very responsive to questions and feedback, ensuring that
+        students have all the resources they need to succeed in the class. Overall, Meg's expertise,
+        teaching skills, and supportive attitude make her an excellent instructor for the
+        web-development class at Eastern Washington University."
+      </v-card-text>
+      <v-card-subtitle class="text-subtitle-1 font-italic">
+        - ChatGPT 3.5, April 2023
+      </v-card-subtitle>
+    </v-card>
+    <div align="center" style="margin-top: 2rem">
+      <v-btn color="#76ff03" variant="outlined" class="mr-5" to="/wordle"> Play The Game </v-btn>
+      <v-btn color="#76ff03" variant="outlined" to="/"> Return Home </v-btn>
+    </div>
   </div>
 </template>
