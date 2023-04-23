5c8a71c7fe43ef0fb235c7b9388c6f968b6e2720
Neriah Graham <ngraham7@ewu.edu>
2023-04-22 19:01:12 -0700

    Added gradient

--- wordle-web/src/components/LetterBase.vue
+++ wordle-web/src/components/LetterBase.vue
@@ -1,10 +1,24 @@
 <template>
-  <v-btn margin="0px" :color="props.color" :elevation="6">
-    {{ props.char }}
-  </v-btn>
+  <v-card :color="props.color">
+    <v-btn id=gradient :elevation="0">
+        {{ props.char }}
+    </v-btn>
+  </v-card>
 </template>
 
+<style scoped>
+  #gradient {
+    background-image: linear-gradient(
+      to bottom right,
+      rgba(0, 0, 0, 0.5),
+      rgba(255, 255, 255, 0.5)
+    );
+    background-color: transparent;
+  }
+</style>
+
 <script setup lang="ts">
+
 export interface Props {
   char?: string
   color?: string
@@ -14,4 +28,14 @@ const props = withDefaults(defineProps<Props>(), {
   char: '?',
   color: 'grey'
 })
+
+
+</script>
+
+<script lang="ts">
+  export default {
+    data: () => ({
+      overlay: true,
+    }),
+  }
 </script>
