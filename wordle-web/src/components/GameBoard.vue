9807abb0e71cf6b86e4c4a55b9c36e974fb19f98
Seth Lanante <73133434+slanante1997@users.noreply.github.com>
2023-04-22 17:10:58 -0700

    Display list of ALL words

--- wordle-web/src/components/GameBoard.vue
+++ wordle-web/src/components/GameBoard.vue
@@ -1,17 +1,22 @@
+<!-- GameBoard.vue -->
 <template>
   <v-row class="justify-center" dense v-for="(word, r) in game.guesses" :key="r">
     <v-col cols="auto" v-for="(letter, c) in word.letters" :key="`${r}-${c}`">
       <LetterButton :letter="letter" @click="letterClick(letter)" />
     </v-col>
   </v-row>
+  <AvailableWords :guessedLetters="game.guessedLetters" />
 </template>
 
 <script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
 import LetterButton from '@/components/LetterButton.vue'
 import type { Letter } from '@/scripts/letter'
 import type { WordleGame } from '@/scripts/wordleGame'
 
-defineProps<{
+const AvailableWords = defineAsyncComponent(() => import('./AvailableWords.vue'))
+
+const props = defineProps<{
   game: WordleGame
 }>()
 
