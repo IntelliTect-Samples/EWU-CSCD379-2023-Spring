ecaff8b19b56ae54916444be42cd271494086541
Ben Knoll <bknoll@ewu.edu>
2023-04-23 20:21:09 -0700

    Deleted HomeView

--- wordle-web/src/router/index.ts
+++ wordle-web/src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import WordleView from '../views/WordleView.vue'
 
 const router = createRouter({
