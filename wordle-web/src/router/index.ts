a7a8c823c511706632f2bd9e92cd838f19e32a6e
grahamna4 <nate.a.graham@gmail.com>
2023-04-23 21:35:54 -0700

    format

--- wordle-web/src/router/index.ts
+++ wordle-web/src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WordleView from '../views/WordleView.vue'
-import HomeViewVue from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,11 +13,6 @@ const router = createRouter({
       path: '/wordle',
       name: 'wordle',
       component: WordleView
-    },
-    {
-      path: '/temp',
-      name: 'validWords',
-      component: HomeViewVue
     },
     {
       path: '/about',
