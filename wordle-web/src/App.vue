096f812de636e66d8afd3aee7b3ab442c3b2f681
Malcolm-Shepherd <mshepherd1@ewu.edu>
2023-04-23 22:34:06 -0700

    Small Fixes

--- wordle-web/src/App.vue
+++ wordle-web/src/App.vue
@@ -14,7 +14,19 @@ import { useTheme } from 'vuetify/lib/framework.mjs'
 
 const theme = useTheme()
 
+function setInverseTheme() {
+  theme.global.name.value = 'inverse'
+}
 
+function setDarkTheme() {
+  theme.global.name.value = 'dark'
+}
+function setKhorneTheme() {
+  theme.global.name.value = 'khorne'
+}
+function setCornTheme() {
+  theme.global.name.value = 'corn'
+}
 
-import {NavBar} from '@/components/NavBar.vue'
+import NavBar from '@/components/NavBar.vue'
 </script>
