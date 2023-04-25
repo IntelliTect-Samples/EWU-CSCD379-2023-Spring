0b5a7a5cfb6496cdc820ee8ccec67ee67f77b3d0
BenFristadSchool <bfristad@ewu.edu>
2023-04-24 19:49:17 -0700

    revert

--- wordle-web/src/components/KeyBoard.vue
+++ wordle-web/src/components/KeyBoard.vue
@@ -1,7 +1,10 @@
 <template>
   <v-row class="justify-center" dense v-for="(key, i) in keyboardLetters" :key="i">
-    <v-col v-for="(char, j) in key" :key="j">
+    <v-col cols="auto" v-for="(char, j) in key" :key="j">
       <LetterButton
+        size="small"
+        height="60"
+        min-width="30"
         :letter="new Letter(char)"
         @click="letterClick(new Letter(char))"
         :elevation="20"
