5698ab5f0cce5c3deb2bad12ce12906c052db684
BenFristadSchool <bfristad@ewu.edu>
2023-04-25 13:44:49 -0700

    added unit tests to wordService

---
+++ wordle-web/src/scripts/__tests__/wordsService.spec.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect } from 'vitest'
+import { WordsService } from '../wordsService'
+import { Word } from '../word'
+
+describe('wordsService', () => {
+  it('will test a value that should return multiple available words', () => {
+    const word = new Word('ball')
+    const expected = ['balls', 'bally']
+    const actual = WordsService.validWords(word.text)
+    expect(expected).toStrictEqual(actual)
+  })
+
+  it('will test a value that should return one available word', () => {
+    const word = new Word('pizz')
+    const expected = ['pizza']
+    const actual = WordsService.validWords(word.text)
+    expect(expected).toStrictEqual(actual)
+  })
+
+  it('will test a value that should return multiple available words', () => {
+    const word = new Word('zzzz')
+    const actual = WordsService.validWords(word.text)
+    expect(actual).toStrictEqual([])
+  })
+})
