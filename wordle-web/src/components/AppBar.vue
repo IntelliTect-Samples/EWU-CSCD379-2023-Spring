46b2e5075852afedc7fc3bd05737dd0fb340c70c
Hunter T <strangeranger01@randomserver.xyz>
2023-04-26 01:05:13 -0700

    Format file using prettier

--- wordle-web/src/components/AppBar.vue
+++ wordle-web/src/components/AppBar.vue
@@ -66,9 +66,9 @@
   </v-navigation-drawer>
 
   <v-navigation-drawer v-model="menu" location="right" app temporary>
-   <v-list density="compact">
+    <v-list density="compact">
       <v-list-item title="Menu" class="py-6"></v-list-item>
-     <v-divider class="py-3"></v-divider>
+      <v-divider class="py-3"></v-divider>
       <v-list-item>
         <v-btn to="/">Home</v-btn>
       </v-list-item>
