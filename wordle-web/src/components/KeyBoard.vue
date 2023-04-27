92384b34dd929d699443bcd1094147ad3cf2b66a
Seth Lanante <73133434+slanante1997@users.noreply.github.com>
2023-04-27 16:05:04 -0700

    Update KeyBoard.vue

--- wordle-web/src/components/KeyBoard.vue
+++ wordle-web/src/components/KeyBoard.vue
@@ -24,7 +24,7 @@ const keyboardLetters = computed(() => {
   const keyboardKeys = [
     ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
     ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-    ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+    ['z', 'x', 'c', 'v', 'b', 'n', 'm', '?']
   ]
 
   for (let keyboardKey of keyboardKeys) {
