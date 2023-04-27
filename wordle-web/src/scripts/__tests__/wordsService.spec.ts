6cbe2322bdf54b2caa5493c76a06684c592b4a22
Roy Dunn <63774178+roydunn19@users.noreply.github.com>
2023-04-27 15:44:25 -0700

    Update wordle-web/src/scripts/__tests__/wordsService.spec.ts
    Co-authored-by: Alex Lacasella <126521739+Xela-19@users.noreply.github.com>

--- wordle-web/src/scripts/__tests__/wordsService.spec.ts
+++ wordle-web/src/scripts/__tests__/wordsService.spec.ts
@@ -17,7 +17,7 @@ describe('wordsService', () => {
     expect(expected).toStrictEqual(actual)
   })
 
-  it('will test a value that should return multiple available words', () => {
+  it('will test a value that should return no available words', () => {
     const word = new Word('zzzz')
     const actual = WordsService.validWords(word.text)
     expect(actual).toStrictEqual([])
