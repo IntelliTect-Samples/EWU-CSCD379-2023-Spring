a669534f32783fa1b08060873186036a5101ec55
FenrisStargazer <80987915+FenrisStargazer@users.noreply.github.com>
2023-04-26 23:14:07 -0700

    Minor change

---
+++ assignment2/src/components/__tests__/SuggestionMenu.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest'
+import SuggestionMenu from '../SuggestionMenu.vue'
+
+/* describe('Letter', () => {
+    it('Gets Correct Color', () => {
+        const Letter = new letter('a')
+        Letter.status = LetterStatus.Correct
+        expect(Letter.color).toBe('green')
+    })
+}) */
