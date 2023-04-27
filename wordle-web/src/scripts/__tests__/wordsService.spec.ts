57814ea2ea47a2702700ef2fabec14b34071f08c
Helaman99 <80353517+arose-13@users.noreply.github.com>
2023-04-27 11:52:13 -0700

    WordService tests

--- wordle-web/src/scripts/__tests__/wordsService.spec.ts
+++ wordle-web/src/scripts/__tests__/wordsService.spec.ts
@@ -2,10 +2,17 @@ import { describe, it, expect } from 'vitest'
 import { WordsService } from '@/scripts/wordsService'
 
 describe('validWords', () => {
+  it('returns 4 valid words starting with aa', () => {
+    const word = 'aahed'
+    const guess = 'aa---'
+    const list: string[] = WordsService.validWords(guess, word)
+    expect(list.length).toEqual(4)
+  }),
   it('returns one valid word', () => {
     const word = 'aahed'
-    const list: string[] = WordsService.validWords(word)
+    const guess = 'aahed'
+    const list: string[] = WordsService.validWords(guess, word)
     expect(list.length).toEqual(1)
-    expect(list[0]).toEqual(word)
+    expect(list[0]).toEqual('aahed')
   })
 })
