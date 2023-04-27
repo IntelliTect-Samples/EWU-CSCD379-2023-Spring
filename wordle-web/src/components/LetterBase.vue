ee44a2a3da3ea218e5b6d5173d521d30db428bdc
Seth Lanante <73133434+slanante1997@users.noreply.github.com>
2023-04-27 15:05:34 -0700

    Added gradient for letter after guess is made

--- wordle-web/src/components/LetterBase.vue
+++ wordle-web/src/components/LetterBase.vue
@@ -1,34 +1,30 @@
+<!-- LetterBase.vue -->
 <template>
-  <v-btn class="shadow-5, elevation-10" id="gradient" :color="props.color">
+  <v-btn class="shadow-5 elevation-10" :style="{ background: keyGradient }">
     {{ props.char }}
   </v-btn>
 </template>
 
-<style scoped>
-#gradient {
-  background: linear-gradient(to right, #e1e1e1, #747474);
-}
-</style>
-
 <script setup lang="ts">
-// Working on implementing the gradient background for the letter buttons
-// import { computed } from 'vue'
+import { computed } from 'vue'
+
+const keyGradient = computed(() => {
+  switch (props.color) {
+    case 'correct':
+      return 'linear-gradient(to right, rgba(0, 128, 0, 0.3), rgba(0, 128, 0, 0.6))'
+    case 'misplaced':
+      return 'linear-gradient(to right, rgba(255, 255, 0, 0.3), rgba(255, 255, 0, 0.6))'
+    case 'wrong':
+      return 'linear-gradient(to right, rgba(255, 0, 0, 0.3), rgba(255, 0, 0, 0.6))'
+    default:
+      return 'linear-gradient(to right, rgba(128, 128, 128, 0.3), rgba(128, 128, 128, 0.6))'
+  }
+})
+
 export interface Props {
   char?: string
   color?: string
 }
-
-// const myGradient = computed(() => {
-//   if (props.color == "misplaced") {
-//     return 'linear-gradient(to right, #ff44AC, #ff5e62)'
-//   }
-//   if (props.color == "correct") {
-//     return 'linear-gradient(to right, #00f2fe, #4facfe)'
-//   }
-//   if (props.color == "wrong") {
-//     return 'linear-gradient(to right, #ff9966, #ff5e62)'
-//   }
-// })
 
 const props = withDefaults(defineProps<Props>(), {
   char: '?',
