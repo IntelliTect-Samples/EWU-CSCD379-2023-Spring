993c3540d489c32a8aaabad54082ee858a0ec45a
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2023-04-27 20:42:33 -0700

    created tests file and made tests to check: 1. Displays a single word if 5 letters are entered an word is in wordlist 2. If word not found in wordlist entered, display no words 3. If no letters entered, display all words 4. partial guesses

---
+++ wordle-web/src/scripts/__tests__/wordsService.spec.ts
@@ -0,0 +1,30 @@
+import { describe, it, expect } from 'vitest'
+import { WordsService } from '../wordsService'
+
+describe('WordsService', () => {
+  it('Displays a single word if 5 letters are entered an word is in wordlist', () => {
+    const validWords = WordsService.validWords('PEACH')
+    expect(validWords.length === 1).toBe(true)
+  })
+
+  it('If word not found in wordlist entered, display no words', () => {
+    const validWords = WordsService.validWords('BEZZS')
+    expect(validWords.length === 0).toBe(true)
+  })
+
+  it('If no letters entered, display all words', () => {
+    const validWords = WordsService.validWords('')
+    expect(validWords.length === 12972).toBe(true)
+  })
+
+  it('Checks partial guesses based on letters already entered', () => {
+    const partialGuess = 'PEA'
+    const validWords = WordsService.validWords(partialGuess)
+    expect(validWords.some((word) => word.text === 'PEACH')).toBe(true)
+    expect(validWords.some((word) => word.text === 'PEARL')).toBe(true)
+    // Incorrect tests
+    expect(validWords.some((word) => word.text === 'PEEPS')).toBe(false)
+    expect(validWords.some((word) => word.text === 'PROPS')).toBe(false)
+
+  })
+})
