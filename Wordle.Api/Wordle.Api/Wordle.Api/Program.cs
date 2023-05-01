abf6f6b26da5806df8cb615f3c49404683d98e85
Malcolm-Shepherd <supermowkitty@gmail.com>
2023-05-01 11:31:12 -0700

    Database Communication

--- Wordle.Api/Wordle.Api/Wordle.Api/Program.cs
+++ Wordle.Api/Wordle.Api/Wordle.Api/Program.cs
@@ -2,24 +2,23 @@ var MyAllowAllOrigins = "_myAllowAllOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-builder.Services.AddCors(options => 
+builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowAllOrigins,
-        policy =>
-        {
-            policy.WithOrigins("*");
-        });
+                      policy =>
+                      {
+                          policy.WithOrigins("*");
+                      });
 });
+
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -34,5 +33,3 @@ app.UseCors(MyAllowAllOrigins);
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.Run();
