c7d5867cee6cfcce8fd8c131746b4b4930b9c0b5
rjdgamer <40226268+rjdgamer@users.noreply.github.com>
2023-05-01 11:27:58 -0700

    Adding Wordle.Api Sln

--- Wordle.Api/Wordle.Api/Controllers/WeatherForecastController.cs
+++ Wordle.Api/Wordle.Api/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Wordle.Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Drizling"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -24,7 +24,7 @@ namespace Wordle.Api.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(5, 55),
+                TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
