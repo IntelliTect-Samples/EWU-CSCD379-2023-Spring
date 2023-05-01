13057195a8c969622e9ed970b2f0f156fdda6c83
Malcolm-Shepherd <supermowkitty@gmail.com>
2023-05-01 11:14:52 -0700

    Allow Servers to Communicate with Eachother

--- Wordle.Api/Wordle.Api/Wordle.Api/Program.cs
+++ Wordle.Api/Wordle.Api/Wordle.Api/Program.cs
@@ -1,5 +1,16 @@
+var MyAllowAllOrigins = "_myAllowAllOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy(name: MyAllowAllOrigins,
+        policy =>
+        {
+            policy.WithOrigins("*");
+        });
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -17,6 +28,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowAllOrigins);
 
 app.UseAuthorization();
 
