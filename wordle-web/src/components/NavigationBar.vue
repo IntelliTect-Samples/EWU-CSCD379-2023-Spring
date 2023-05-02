3ba8e279c34398ce1580876c5ea4d4004683d0b8
alowe4 <117127121+alowe4@users.noreply.github.com>
2023-05-02 12:45:15 -0700

    added assignment 3 part 4 face

--- wordle-web/src/components/NavigationBar.vue
+++ wordle-web/src/components/NavigationBar.vue
@@ -10,27 +10,37 @@
           <router-link to="/"
             ><v-app-bar-title class="primary" color="primary"> wordle</v-app-bar-title></router-link
           >
-          <SettingsDialogVue />
         </template>
         <v-spacer></v-spacer>
 
-        <template v-slot:append>
-          <v-btn>
-            <v-menu>
-              <template v-slot:activator="{ props }">
-                <v-btn v-bind="props" icon="mdi-hamburger"> </v-btn>
-              </template>
+        <v-card>
+          <router-link to="/leaderBoard"><v-btn>Link To leaderBoard</v-btn></router-link>
+        </v-card>
 
-              <v-list>
-                <router-link to="/about"
-                  ><v-list-item-title>Link To About Page</v-list-item-title></router-link
-                >
-                <router-link to="/wordle"
-                  ><v-list-item-title>Link To Wordle Game</v-list-item-title></router-link
-                >
-              </v-list>
-            </v-menu>
+        <v-card>
+          <v-btn>
+            <SettingsDialogVue />
           </v-btn>
+        </v-card>
+
+        <template v-slot:append>
+          <v-menu>
+            <template v-slot:activator="{ props }">
+              <v-btn v-bind="props" icon="mdi-hamburger"> </v-btn>
+            </template>
+
+            <v-list>
+              <router-link to="/about"
+                ><v-list-item-title>Link To About Page</v-list-item-title></router-link
+              >
+              <router-link to="/wordle"
+                ><v-list-item-title>Link To Wordle Game</v-list-item-title></router-link
+              >
+              <router-link to="/leaderBoard"
+                ><v-list-item-title>Link To LeaderBoard</v-list-item-title></router-link
+              >
+            </v-list>
+          </v-menu>
         </template>
       </v-app-bar>
     </v-layout>
--- wordle-web/src/views/HomeView.vue
+++ wordle-web/src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <main>Home Page</main>
+  <v-card>
+    <router-link to="/leaderBoard"><v-btn>Link To leaderBoard</v-btn></router-link>
+  </v-card>
 </template>
 
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
