e723ddde2677be8467717b70e34491981eac9f07
TannerStephenson <117472530+TannerStephenson@users.noreply.github.com>
2023-05-03 14:28:04 -0700

    UserName component
    It works just not implemented yet so it does not conflict with any changes made upcoming on Thursday. It's just a button that opens a dialog. When implemented. We need to shut down the keyboard for the WordleView.

---
+++ wordle-web/src/components/UserName.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-btn @click.stop="dialog = !dialog">
+        <v-icon>mdi-login</v-icon>
+        {{ UserName }}
+    </v-btn>
+
+    <v-dialog
+        v-model="dialog"
+        transition="dialog-bottom-transition"
+        width="auto"
+        persistent
+      >
+    <v-card class="justify-center text-center" width="400px" height="320px">
+        Enter a user name:
+        <v-text-field label="Label" variant="outlined" v-model="inputUserName" @input="updateUserName"></v-text-field>
+        <v-btn variant="outlined" @click="saveUserName">Enter</v-btn>
+    </v-card>
+    </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const dialog = ref(false)
+const UserName = ref('guest')
+const inputUserName = ref('')
+
+function updateUserName() {
+  UserName.value = inputUserName.value
+}
+
+function saveUserName() {
+  UserName.value = inputUserName.value
+  dialog.value = false
+}
+
+function clearInput() {
+  inputUserName.value = ''
+}
+</script>
