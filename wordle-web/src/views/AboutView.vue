125b841db5f489297f2ce9d9afa9ec2db9a06d7a
Joshua <joshuahess11@gmail.com>
2023-05-03 16:18:24 -0700

    Update

--- wordle-web/src/views/AboutView.vue
+++ wordle-web/src/views/AboutView.vue
@@ -6,7 +6,7 @@
   </v-sheet>
 
   {{ isDialogOpen }}
-  <WeatherDialog v-model="isDialogOpen" :weather="currentWeather"></WeatherDialog>
+  <WeatherDialog v-model="isDialogOpen" :weather="currentWeather!"></WeatherDialog>
 
   <v-card v-for="item in weatherData" :key="item.date" @click="setCurrentWeather(item)">
     {{ item.date }}</v-card
@@ -18,16 +18,13 @@ import Axios from 'axios'
 import { ref } from 'vue'
 import WeatherDialog from '@/components/WeatherDialog.vue'
 import type { WeatherData } from '@/types/WeatherData'
-
 const isDialogOpen = ref(false)
 const weatherData = ref<WeatherData[]>()
 const currentWeather = ref<WeatherData>()
-
 function setCurrentWeather(weather: WeatherData) {
   currentWeather.value = weather
   isDialogOpen.value = true
 }
-
 Axios.get('https://localhost:7053/WeatherForecast')
   .then((response) => {
     console.log(response.data)
