3108285825ff8afae79300466a1b7da9332652fd
rjdgamer <40226268+rjdgamer@users.noreply.github.com>
2023-05-03 20:20:57 -0700

    Cute lil message

--- wordle-web/src/views/AboutView.vue
+++ wordle-web/src/views/AboutView.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div>
     <h1>This is an about page</h1>
     <p>
@@ -20,45 +19,6 @@ import $router from '../router/index'
 function goBack() {
   $router.go(-1)
 }
-=======
-  <v-sheet color="blue" height="170px" class="pt-10 my-5">
-    <v-card color="indigo-darken-3" class="ma-5" elevation="5" rounded="xl">
-      <v-card-title>Hello World</v-card-title>
-    </v-card>
-  </v-sheet>
-
-  {{ isDialogOpen }}
-  <WeatherDialog v-model="isDialogOpen" :weather="currentWeather!"></WeatherDialog>
-
-  <v-card v-for="item in weatherData" :key="item.date" @click="setCurrentWeather(item)">
-    {{ item.date }}</v-card
-  >
-</template>
-
-<script setup lang="ts">
-import Axios from 'axios'
-import { ref } from 'vue'
-import WeatherDialog from '@/components/WeatherDialog.vue'
-import type { WeatherData } from '@/types/WeatherData'
-
-const isDialogOpen = ref(false)
-const weatherData = ref<WeatherData[]>()
-const currentWeather = ref<WeatherData>()
-
-function setCurrentWeather(weather: WeatherData) {
-  currentWeather.value = weather
-  isDialogOpen.value = true
-}
-
-Axios.get('https://localhost:7053/WeatherForecast')
-  .then((response) => {
-    console.log(response.data)
-    weatherData.value = response.data
-  })
-  .catch((err) => {
-    console.log(err)
-  })
->>>>>>> 65d410439297113ad6946631cf981f206191fc49
 </script>
 
 <style>
