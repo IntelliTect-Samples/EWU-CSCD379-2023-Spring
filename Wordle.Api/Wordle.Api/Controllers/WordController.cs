7263e140661b119974ffa336da24a5f4e7dbb4e3
Nick Parkman <nparkman@ewu.edu>
2023-05-04 14:22:49 -0700

    set up for Assignment 3 PR

---
+++ Wordle.Api/Wordle.Api/Controllers/WordController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Wordle.Api.Data;
+using Wordle.Api.Services;
+
+namespace Wordle.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WordController : ControllerBase
+    {
+        private readonly WordService _wordService;
+
+        public WordController(WordService wordService)
+        {
+            _wordService = wordService;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return await _wordService.GetRandomWord();
+        }
+    }
+}
+
---
+++ Wordle.Api/Wordle.Api/Data/Word.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Wordle.Api.Data
+{
+    public class Word
+    {
+        public int WordId { get; set; }
+        public required string Text { get; set; }
+        public bool IsCommon { get; set; }
+        
+        public static void SeedWords(AppDbContext db)
+        {
+            if (!db.Words.Any())
+            {
+                var wordLines = System.IO.File.ReadAllLines("Content/Words.csv");
+                foreach (var line in wordLines)
+                {
+                    var parts = line.Split(',');
+                    var word = new Word()
+                    {
+                        Text = parts[0],
+                        IsCommon = bool.Parse(parts[1])
+                    };
+                    db.Words.Add(word);
+                }
+                db.SaveChanges();
+            }
+        }
+    }
+}
