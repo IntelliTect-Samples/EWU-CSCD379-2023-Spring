0ebc3f8e42435a404a2e342e224dbeef708ab961
Ian Kaiserman <ian.kaiserman@gmail.com>
2023-05-04 14:55:55 -0700

    Prettier format run

--- wordle-web/src/components/UserName.vue
+++ wordle-web/src/components/UserName.vue
@@ -1,21 +1,21 @@
 <template>
-    <v-btn @click.stop="dialog = !dialog">
-        <v-icon>mdi-login</v-icon>
-        {{ UserName }}
-    </v-btn>
+  <v-btn @click.stop="dialog = !dialog">
+    <v-icon>mdi-login</v-icon>
+    {{ UserName }}
+  </v-btn>
 
-    <v-dialog
-        v-model="dialog"
-        transition="dialog-bottom-transition"
-        width="auto"
-        persistent
-      >
+  <v-dialog v-model="dialog" transition="dialog-bottom-transition" width="auto" persistent>
     <v-card class="justify-center text-center" width="400px" height="320px">
-        Enter a user name:
-        <v-text-field label="Label" variant="outlined" v-model="inputUserName" @input="updateUserName"></v-text-field>
-        <v-btn variant="outlined" @click="saveUserName">Enter</v-btn>
+      Enter a user name:
+      <v-text-field
+        label="Label"
+        variant="outlined"
+        v-model="inputUserName"
+        @input="updateUserName"
+      ></v-text-field>
+      <v-btn variant="outlined" @click="saveUserName">Enter</v-btn>
     </v-card>
-    </v-dialog>
+  </v-dialog>
 </template>
 
 <script setup lang="ts">
