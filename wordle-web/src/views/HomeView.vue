49333ba2f4b79149bf857ec6e6dcf302eb318006
alowe4 <117127121+alowe4@users.noreply.github.com>
2023-05-04 11:38:31 -0700

    added link to about on HomeView

--- wordle-web/src/views/HomeView.vue
+++ wordle-web/src/views/HomeView.vue
@@ -3,6 +3,7 @@
   <v-card>
     <router-link to="/leaderBoard"><v-btn>Link To leaderBoard</v-btn></router-link>
     <router-link to="/wordle"><v-btn>Link To game</v-btn></router-link>
+    <router-link to="/about"><v-btn>Link To about</v-btn></router-link>
   </v-card>
 </template>
 
