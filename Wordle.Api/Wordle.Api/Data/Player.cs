48ec1147ddce2ecc78ad6348c84bb4a35946eb05
Helaman99 <80353517+arose-13@users.noreply.github.com>
2023-05-03 21:29:57 -0700

    Create Player.cs

---
+++ Wordle.Api/Wordle.Api/Data/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Wordle.Api.Data;
+
+public class Player
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int PlayerId { get; set; }
+    public string Name { get; set; }
+    public int GameCount { get; set; }
+    public double AverageAttempts { get; set; }
+
+    public Player(string name)
+    {
+        Name = name;
+        GameCount = 0;
+        AverageAttempts = 0;
+    }
+
+    public void UpdateAttempts(int attempts) { AverageAttempts = (AverageAttempts + attempts) / ++GameCount; }
+}
