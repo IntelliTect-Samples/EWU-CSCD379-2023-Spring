5259a12a09d2e34c9454f37ea3991d9dab8139fd
Nick Parkman <nparkman@ewu.edu>
2023-05-04 13:42:18 -0700

    weather dialog and data updates

--- wordle-web/src/components/WeatherDialog.vue
+++ wordle-web/src/components/WeatherDialog.vue
@@ -1,13 +1,18 @@
 <template>
-    <v-dialog v-model="isDialogOpen">
-        <v-card>
-            <v-card-title>Weather or Not</v-card-title>
-        </v-card>
-    </v-dialog>
+  <v-dialog>
+    <v-card>
+      <v-card-title>Weather or Not</v-card-title>
+      <v-card-text>
+        {{ weather.date }} - {{ weather.temperatureC }} - {{ weather.summary }}</v-card-text
+      >
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup lang="ts">
-    import { ref } from 'vue'
+import type { WeatherData } from '@/types/WeatherData'
 
-    const isDialogOpen = ref(false)
+const props = defineProps<{
+  weather: WeatherData
+}>()
 </script>
