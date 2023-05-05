9e97c82a16f2d9300ef7b79fd9ff9b49653b077b
Hunter T <strangeranger01@randomserver.xyz>
2023-05-05 11:14:45 -0700

    Remove code relating to previously removed API

--- wordle-web/src/views/AboutView.vue
+++ wordle-web/src/views/AboutView.vue
@@ -33,39 +33,7 @@
     <br />
     <v-btn to="/wordle" location="center" style="top: 1rem">Return To Game</v-btn>
   </v-sheet>
-
-  {{ isDialogOpen }}
-  <WeatherDialog v-model="isDialogOpen" :weather="currentWeather!"></WeatherDialog>
-
-  <v-card v-for="item in weatherData" :key="item.date" @click="setCurrentWeather(item)">
-    {{ item.date }}</v-card
-  >
 </template>
-
-<script setup lang="ts">
-import Axios from 'axios'
-import { ref } from 'vue'
-import WeatherDialog from '@/components/WeatherDialog.vue'
-import type { WeatherData } from '@/types/WeatherData'
-
-const isDialogOpen = ref(false)
-const weatherData = ref<WeatherData[]>()
-const currentWeather = ref<WeatherData>()
-
-function setCurrentWeather(weather: WeatherData) {
-  currentWeather.value = weather
-  isDialogOpen.value = true
-}
-
-Axios.get('https://localhost:7053/WeatherForecast')
-  .then((response) => {
-    console.log(response.data)
-    weatherData.value = response.data
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-</script>
 
 <style>
 @media (min-width: 1024px) {
