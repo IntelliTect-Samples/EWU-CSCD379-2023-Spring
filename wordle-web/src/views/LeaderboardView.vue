b377c0a5b28e27e686d84d611ae0ae803b1ef18a
Eric Vo <tvo12@ewu.edu>
2023-05-06 14:35:13 -0700

    basic leaderboard frame

--- wordle-web/src/views/LeaderboardView.vue
+++ wordle-web/src/views/LeaderboardView.vue
@@ -2,13 +2,37 @@
   <v-sheet>
     <v-card>
       <v-card-title>Leader Board</v-card-title>
-      <p>Test</p>
+      <div> 
+        <p>First</p>
+        <v-progress-linear 
+        v-model="progress"
+        color="amber"
+        height="25"
+        striped
+      ><strong>First place</strong> </v-progress-linear>
+      </div>
+
+
       <v-btn @click="$router.back()">Back</v-btn>
     </v-card>
   </v-sheet>
-  <body>
-    <p>test1</p>
-  </body>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const progress = ref(80)
+
+</script>
+
+<style scoped>
+ .v-progress-linear {
+  border-radius: 8px;
+  width: 99%;
+}
+
+</style>
+```
+
+
+
