5e4c1996645a2c0e124409ceadf9da2aff02ab8e
Eric Vo <tvo12@ewu.edu>
2023-05-06 14:09:04 -0700

    Fix Narbar, style, router

--- wordle-web/src/router/index.ts
+++ wordle-web/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import WordleView from '../views/WordleView.vue'
+import LeaderboardView from '../views/LeaderboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,12 @@ const router = createRouter({
       name: 'wordle',
       component: WordleView
     },
+    {
+      path: '/leaderboard',
+      name: 'leaderboard',
+      component: LeaderboardView
+    }
+    ,
     {
       path: '/about',
       name: 'about',
