f5cfa1bf963851aac8dc01b2581990c3c58589ac
Eric Vo <tvo12@ewu.edu>
2023-05-06 15:40:22 -0700

    15 lines leaderboard need name + score

--- wordle-web/src/views/LeaderboardView.vue
+++ wordle-web/src/views/LeaderboardView.vue
@@ -2,16 +2,28 @@
   <v-sheet>
     <v-card>
       <v-card-title>Leader Board</v-card-title>
-      <div> 
-        <p>First</p>
-        <v-progress-linear 
-        v-model="progress"
+      <v-progress-linear 
+        v-model="progress[0]"
         color="amber"
         height="25"
-        striped
-      ><strong>First place</strong> </v-progress-linear>
-      </div>
-
+      ><strong>First place (Me)</strong> </v-progress-linear>
+      <v-progress-linear 
+        v-model="progress[1]"
+        color="#AFAFAF"
+        height="25"
+      ><strong>Second place (Also Me)</strong> </v-progress-linear>
+      <v-progress-linear 
+        v-model="progress[2]"
+        color="#CD7F32"
+        height="25"
+      ><strong>Third place (Still Me)</strong> </v-progress-linear>
+      <template v-for="i in 17" :key="i.id">
+        <v-progress-linear 
+          v-model="progress[i+3]"
+          color="#757575"
+          height="25"
+        ></v-progress-linear>
+      </template>
 
       <v-btn @click="$router.back()">Back</v-btn>
     </v-card>
@@ -21,7 +33,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const progress = ref(80)
+const progress = ref([100,80,60,40,20,18,16,14,18,10])
 
 </script>
 
@@ -29,8 +41,8 @@ const progress = ref(80)
  .v-progress-linear {
   border-radius: 8px;
   width: 99%;
+  margin-bottom: 10px;
 }
-
 </style>
 ```
 
