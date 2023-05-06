f24609f4c42e08798092d419eb6ed2bbbc9c15af
VeraKorchemnaya <vkorchemnaya@ewu.edu>
2023-05-06 12:29:29 -0700

    Create Player Class

---
+++ Wordle.Api/Wordle.Api/Data/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace Wordle.Api.Data
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public required string Name { get; set; }
+        public int GameCount { get; set; }
+        public double AverageAttempts { get; set; }
+        public int AveargeSecondsPerGame { get; set; }
+    }
+}
