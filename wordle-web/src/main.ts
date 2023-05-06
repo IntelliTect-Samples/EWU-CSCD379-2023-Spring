b996a066437602f95c692d8fb46ae2b99a00a2be
rjdgamer <40226268+rjdgamer@users.noreply.github.com>
2023-05-06 10:06:12 -0700

    Fixing Azure

--- wordle-web/src/main.ts
+++ wordle-web/src/main.ts
@@ -17,7 +17,7 @@ import Axios from 'axios'
 if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
   Axios.defaults.baseURL = 'https://localhost:7053/'
 } else {
-  Axios.defaults.baseURL = 'wordletest2023.azurewebsites.net/'
+  Axios.defaults.baseURL = 'wordlewebspring2023.azurewebsites.net'
 }
 
 const vuetify = createVuetify({
