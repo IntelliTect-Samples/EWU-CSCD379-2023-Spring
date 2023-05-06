e44e5c3ed15ef4ea8d430206d61fcf9a37fd4591
Cayden Kappes <caydenkappes@gmail.com>
2023-05-06 11:17:05 -0700

    Changes from class

--- Wordle.Api/Wordle.Api/Services/WordService.cs
+++ Wordle.Api/Wordle.Api/Services/WordService.cs
@@ -6,18 +6,60 @@ namespace Wordle.Api.Services
     public class WordService
     {
         private readonly AppDbContext _db;
-        
+
         public WordService(AppDbContext db)
         {
             _db = db;
         }
-        
+
         public async Task<string> GetRandomWord()
         {
             var count = await _db.Words.CountAsync(word => word.IsCommon);
-            var index = new System.Random().Next(count);
-            var word = await _db.Words.Where(word => word.IsCommon).Skip(index).FirstAsync();
+            var index = new Random().Next(count);
+            var word = await _db.Words
+                .Where(word => word.IsCommon)
+                .Skip(index)
+                .FirstAsync();
             return word.Text;
+        }
+
+        public async Task<IEnumerable<Word>> GetSeveralWords(int? count)
+        {
+            count ??= 10;
+            var totalCount = await _db.Words.CountAsync(word => word.IsCommon);
+            totalCount -= count.Value;
+            int index = new Random().Next(totalCount);
+            var words = await _db.Words
+                .Where(word => word.IsCommon)
+                .Skip(index)
+                .Take(count.Value)
+                .OrderByDescending(w => w.Text)
+                .ToListAsync();
+            return words;
+        }
+
+        public async Task<Word> AddWord(string? newWord, bool isCommon)
+        {
+            if (newWord is null || newWord.Length != 5)
+            {
+                throw new ArgumentException("Word must be 5 characters long");
+            }
+            var word = await _db.Words.FirstOrDefaultAsync(w => w.Text == newWord);
+            if (word != null)
+            {
+                word.IsCommon = isCommon;
+            }
+            else
+            {
+                word = new()
+                {
+                    Text = newWord,
+                    IsCommon = isCommon
+                };
+                _db.Words.Add(word);
+            }
+            await _db.SaveChangesAsync();
+            return word;
         }
     }
 }
