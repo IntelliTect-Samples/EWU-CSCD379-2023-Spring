e281cfbd3664a9055c5b6da75b7495543a75a4fd
roydunn19 <roydunn100@gmail.com>
2023-05-07 11:33:29 -0700

    fixes for data types

--- Wordle.Api/Wordle.Api/Data/Player.cs
+++ Wordle.Api/Wordle.Api/Data/Player.cs
@@ -6,8 +6,8 @@
         public required string PlayerName { get; set; }
         public int GameCount { get; set; }
         public int TotalTimeInSeconds { get; set; }
-        public int AverageTimeInSeconds { get; set; }
-        public double TotalAttempts { get; set; }
+        public int? AverageTimeInSeconds { get; set; }
+        public double? TotalAttempts { get; set; }
         public double AverageAttempts { get; set; }
     }
 }
