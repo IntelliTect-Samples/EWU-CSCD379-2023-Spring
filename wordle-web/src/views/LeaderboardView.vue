50e7ab86c81c4cf60b0f935b026c92612765971f
Cayden Kappes <caydenkappes@gmail.com>
2023-05-07 10:10:37 -0700

    Basic Leaderboard

---
+++ wordle-web/src/views/LeaderboardView.vue
@@ -0,0 +1,46 @@
+<template>
+    <h1>Leaderboard</h1>
+    <v-row>
+        <v-col>
+            Player Name:
+        </v-col>
+        <v-col>
+            Average Number of Attempts:
+        </v-col>
+        <v-col>
+            Average Seconds Per Game:
+        </v-col>
+    </v-row>
+    <v-row v-for="player in leaderboard">
+        <v-col>
+            {{player['name']}}
+        </v-col>
+        <v-col>
+            {{player['averageAttempts']}}
+        </v-col>
+        <v-col>
+            {{player['averageSecondsPerGame']}}
+        </v-col>
+    </v-row>
+    
+</template>
+  
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import Axios from 'axios'
+  
+  var leaderboard = ref([])
+
+  Axios.get('Leaderboard/GetTopPlayers')
+      .then((response) => {
+        console.log(response.data)
+        leaderboard = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+  
+  
+</script>
+  
