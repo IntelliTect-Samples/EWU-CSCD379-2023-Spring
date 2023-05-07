c28dab4cd3fad6787dab819fbd80882ab67560f5
Nick Parkman <nparkman@ewu.edu>
2023-05-06 19:42:58 -0700

    Leader Board Api update

--- Wordle.Api/Wordle.Api/Services/PlayerService.cs
+++ Wordle.Api/Wordle.Api/Services/PlayerService.cs
@@ -12,10 +12,39 @@ namespace Wordle.Api.Services
         {
             _db = db;
         }
-
+        /*
         public async Task<IEnumerable<Player>> GetPlayer()
         {
             
+        }
+        */
+        public async Task<Player> AddPlayer(string? newName, int? playTime)
+        {
+            if(newName == null) { throw new ArgumentException("Name can't be null"); }
+            var player = await _db.Players.FirstOrDefaultAsync(p =>  p.Name == newName);
+            if(player != null) 
+            {
+                player.GameCount++;
+                player.TotalAttempts++;
+                player.AverageAttempts = player.TotalAttempts / player.GameCount;
+                player.TotalSecoundsPlayed += playTime;
+                player.AverageSecondsPerGame = player.TotalSecoundsPlayed / player.GameCount;
+            }
+            else
+            {
+                player = new()
+                {
+                    name = newName,
+                    GameCount = 1,
+                    TotalAttempts = 1,
+                    AverageAttempts = 1,
+                    TotalSecoundsPlayed = playTime,
+                    AverageSecondsPerGame = playTime
+                };
+                db.Players.Add(player);
+            }
+            await _db.SaveChangesAsync();
+            return player;
         }
 
     }
