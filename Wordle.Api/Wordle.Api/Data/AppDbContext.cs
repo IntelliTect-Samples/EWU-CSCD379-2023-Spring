b6a26bdc74469d9b0f27d73a4e7f7f361a083dda
roydunn19 <roydunn100@gmail.com>
2023-05-07 12:53:24 -0700

    auto gen for keys

--- Wordle.Api/Wordle.Api/Data/AppDbContext.cs
+++ Wordle.Api/Wordle.Api/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 
 namespace Wordle.Api.Data
 {
@@ -7,6 +8,13 @@ namespace Wordle.Api.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Player>()
+            .Property(f => f.PlayerId)
+            .ValueGeneratedOnAdd();
         }
 
         public DbSet<Word> Words => Set<Word>();
--- Wordle.Api/Wordle.Api/Data/Player.cs
+++ Wordle.Api/Wordle.Api/Data/Player.cs
@@ -1,7 +1,13 @@
-﻿namespace Wordle.Api.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace Wordle.Api.Data
 {
     public class Player
     {
+        [Key]
         public int PlayerId { get; set; }
         public required string Name { get; set; }
         public int GameCount { get; set; }
