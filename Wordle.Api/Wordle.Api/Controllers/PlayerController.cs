15e63a7a398bfea2e9564de5a0fec0f76d002767
TannerStephenson <117472530+TannerStephenson@users.noreply.github.com>
2023-05-07 12:41:08 -0700

    Auto stash before merge of "Assignment-3" and "origin/Assignment-3"

---
+++ Wordle.Api/Wordle.Api/Controllers/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wordle.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayerController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<Player> AddNewPlayer(int playerID, string name, int gameCount, int averageAttempts)
+        {
+            //Add the players score to the database
+            return await Task.FromResult(new Player
+            {
+                PlayerId = playerID,
+                Name = name,
+                GameCount = gameCount,
+                AverageAttempts = averageAttempts
+            }) ;
+
+        }
+    }
+}
---
+++ Wordle.Api/Wordle.Api/Controllers/PlayersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wordle.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<Players>> Get()
+        {
+            return await Task.FromResult(Enumerable.Range(1, 10).Select(index => new Players
+            {
+                PlayerId = index,
+                Name = $"Player {index}",
+                GameCount = Random.Shared.Next(1, 100),
+                AverageAttempts = Random.Shared.Next(1, 100)
+            })
+                           .ToArray());
+        }
+    }
+}
