fcc4e65d5ab0340f53e289391bce61612b3d1a1e
Nolan Posey <nposey1@ewu.edu>
2023-05-07 20:22:05 -0700

    Added Axios call for top ten scores to leaderboard

--- wordle-web/src/components/BoardDisplay.vue
+++ wordle-web/src/components/BoardDisplay.vue
@@ -5,6 +5,9 @@
       <v-carousel-item width="700px">
         <v-card class="text-center">
           <v-card-title class="display-1"> LeaderBoard </v-card-title>
+          <v-list v-for="player in players" >
+            {{ player }}
+          </v-list>
         </v-card>
       </v-carousel-item>
       <v-carousel-item width="700px">
@@ -19,9 +22,21 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import Axios from 'axios'
 
 let dialog = ref(true)
 let username = ref(localStorage.getItem('username') || 'Guest')
+
+let players = new Array<string>()
+
+Axios.get('leaderboard/GetTopTenScores')
+  .then(response => {
+    players = response.data
+    console.log(response.data)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 </script>
 
 <style>
