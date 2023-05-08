c78144561b28c4badc316e3944c375101c56ad98
Nolan Posey <nposey1@ewu.edu>
2023-05-08 16:18:12 -0700

    Leaderboard displays top ten scors in JSON form.

--- wordle-web/src/components/BoardDisplay.vue
+++ wordle-web/src/components/BoardDisplay.vue
@@ -5,8 +5,8 @@
       <v-carousel-item width="700px">
         <v-card class="text-center">
           <v-card-title class="display-1"> LeaderBoard </v-card-title>
-          <v-list v-for="player in players" >
-            {{ player }}
+          <v-list v-for="(player, i) in players" :key="i">
+            {{ players[i] }}
           </v-list>
         </v-card>
       </v-carousel-item>
@@ -26,12 +26,20 @@ import Axios from 'axios'
 
 let dialog = ref(true)
 let username = ref(localStorage.getItem('username') || 'Guest')
-
 let players = new Array<string>()
 
 Axios.get('leaderboard/GetTopTenScores')
   .then(response => {
-    players = response.data
+    players[0] = response.data[0]
+    players[1] = response.data[1]
+    players[2] = response.data[2]
+    players[3] = response.data[3]
+    players[4] = response.data[4]
+    players[5] = response.data[5]
+    players[6] = response.data[6]
+    players[7] = response.data[7]
+    players[8] = response.data[8]
+    players[9] = response.data[9]
     console.log(response.data)
   })
   .catch((error) => {
