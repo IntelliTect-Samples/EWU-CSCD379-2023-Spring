97741057888d4440fc7d78037a97d6148716db03
AndrewKorchemniy <andrewkorch@gmail.com>
2023-05-07 21:01:49 -0700

    Implement leaderboard view

--- wordle-web/src/App.vue
+++ wordle-web/src/App.vue
@@ -4,7 +4,7 @@
       <nav>
         <RouterLink to="/">Home</RouterLink> | <RouterLink to="/wordle">Wordle</RouterLink> |
         <RouterLink to="/about">About</RouterLink> |
-        <RouterLink to="/leaderboard">Leader Board</RouterLink>
+        <RouterLink to="/leaderboard">Leaderboard</RouterLink>
       </nav>
       <v-btn @click="setInverseTheme"> Inverse Theme </v-btn>
       <v-btn @click="setDarkTheme"> Dark Theme </v-btn>
--- wordle-web/src/views/LeaderBoardView.vue
+++ wordle-web/src/views/LeaderBoardView.vue
@@ -1,3 +1,49 @@
 <template>
-  <h1>Leader Board</h1>
+  <h1>Leaderboard</h1>
+  <v-table striped class="text-no-wrap">
+    <thead>
+      <tr>
+        <th class="text-left">Name</th>
+        <th class="text-left">Games Played</th>
+        <th class="text-left">Average Attempts</th>
+        <th class="text-left">Average Time</th>
+        <th class="w-100"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in leaderboardData" :key="item.name">
+        <td>{{ item.name }}</td>
+        <td>{{ item.gameCount }}</td>
+        <td>{{ item.averageAttempts }}</td>
+        <td>{{ item.averageSecondsPerGame }}</td>
+        <td class="w-100"></td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import Axios from 'axios'
+
+interface LeaderboardData {
+  playerId: number
+  name: string
+  gameCount: number
+  averageAttempts: number
+  averageSecondsPerGame: number
+  weightedScore: number
+}
+
+const leaderboardUrl = 'https://wordlemindbender.azurewebsites.net/Player/GetTopTenPlayers'
+
+const leaderboardData = ref<LeaderboardData[]>()
+Axios.get(leaderboardUrl)
+  .then((response) => {
+    console.log(response.data)
+    leaderboardData.value = response.data
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+</script>
