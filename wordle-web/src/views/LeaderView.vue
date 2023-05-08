b9101be037d7b20e1653f4f099b81a8e5b650320
TannerStephenson <117472530+TannerStephenson@users.noreply.github.com>
2023-05-07 21:39:59 -0700

    Actions fix?

--- wordle-web/src/views/LeaderView.vue
+++ wordle-web/src/views/LeaderView.vue
@@ -1,6 +1,6 @@
 <template>
   <h1 align="center">Top 10 Wordle Scores</h1>
-  <v-row class="justify-center" dense v-for="(leader, index) in LeaderData" :key="leader.playerId">
+  <v-row class="justify-center" dense v-for="(leader, index) in leaderData" :key="leader.playerId">
     <v-col cols="auto">{{ index + 1 }}: {{ leader.name }} - {{ leader.gameCount }} games, {{ leader.averageAttempts }} average attempts</v-col>
   </v-row>
 </template>
@@ -10,7 +10,7 @@ import { ref } from 'vue'
 import Axios from 'axios'
 import type { LeaderData } from '@/types/LeaderData'
 
-const LeaderData = ref<LeaderData[]>()
+const leaderData = ref<LeaderData[]>()
 
 Axios.defaults.baseURL = 'https://kind-stone-01ab5711e.3.azurestaticapps.net'
 if (window.location.hostname === 'localhost') {
@@ -20,7 +20,7 @@ if (window.location.hostname === 'localhost') {
 Axios.get(Axios.defaults.baseURL + '/Leaderboard')
   .then((response) => {
     console.log(response.data)
-    LeaderData.value = response.data
+    leaderData.value = response.data
   })
   .catch((err) => {
     console.log(err)
