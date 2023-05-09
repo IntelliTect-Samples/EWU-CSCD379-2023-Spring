6c4be5ee89345fa6491db6d9ca39354d66d9059a
Seth Lanante <73133434+slanante1997@users.noreply.github.com>
2023-05-09 09:09:08 -0700

    Test deployment

---
+++ Wordle.Api/Wordle.Api/Data/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace Wordle.Api.Data
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public required string Name { get; set; }
+        public int GameCount { get; set; }
+        public double AverageAttempts { get; set; }
+    }
+}
