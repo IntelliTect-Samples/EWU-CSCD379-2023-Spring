14464d78a6939ea5985dc89a4fbd361f3006c698
Joshua <joshuahess11@gmail.com>
2023-05-09 13:59:31 -0700

    prettier check

---
+++ wordle-web/src/components/KeyBoard.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-row class="justify-center" dense v-for="(key, i) in keyboardLetters" :key="i">
+    <v-col class="key-column" v-for="(letter, j) in key" :key="j">
+      <LetterButton :letter="letter" @click="letterClick(letter)" class="key-button" />
+    </v-col>
+  </v-row>
+</template>
+<script setup lang="ts">
+import LetterButton from '@/components/LetterButton.vue'
+import { Letter } from '@/scripts/letter'
+import { computed } from 'vue'
+
+const props = defineProps<{
+  guessedLetters: Letter[]
+}>()
+
+const keyboardLetters = computed(() => {
+  console.log(props.guessedLetters.length)
+  const keyboardLetters: Letter[][] = []
+
+  const keyboardKeys = [
+    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+    ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+  ]
+
+  for (let keyboardKey of keyboardKeys) {
+    let keyboardRow: Letter[] = []
+
+    for (let key of keyboardKey) {
+      keyboardRow.push(props.guessedLetters.find((l) => l.char === key) ?? new Letter(key))
+    }
+
+    keyboardLetters.push(keyboardRow)
+  }
+
+  return keyboardLetters
+})
+
+const emits = defineEmits<{
+  (event: 'letterClick', value: Letter): void
+}>()
+
+function letterClick(letter: Letter) {
+  emits('letterClick', letter)
+}
+</script>
+<style scoped>
+.key-column {
+  padding: 0;
+  margin: 0;
+  flex-grow: 0;
+}
+
+.key-button {
+  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.3);
+}
+.key-column {
+  padding: 0;
+  margin: 0;
+  flex-grow: 0;
+  background: linear-gradient(to right, #5d5f687c, #8b8b86);
+}
+</style>
