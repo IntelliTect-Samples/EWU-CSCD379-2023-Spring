9adce68c88d5544271b85794a0173023cd16d30f
VForev <85714161+VForev@users.noreply.github.com>
2023-05-09 12:09:52 -0700

    deleting error test

--- Wordle.Api/Wordle.Api/Program.cs
+++ Wordle.Api/Wordle.Api/Program.cs
@@ -39,7 +39,6 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
     Word.SeedWords(db);
     Player.SeedPlayers(db);
 }
