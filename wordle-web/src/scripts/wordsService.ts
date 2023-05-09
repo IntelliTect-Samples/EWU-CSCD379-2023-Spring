c68f15b071c4df2063ed58cbece4565c0b46d7db
FenrisStargazer <80987915+FenrisStargazer@users.noreply.github.com>
2023-05-09 16:28:39 -0700

    Trying to understand

--- wordle-web/src/scripts/wordsService.ts
+++ wordle-web/src/scripts/wordsService.ts
@@ -5,14 +5,15 @@ export abstract class WordsService {
     return this.#words[Math.floor(Math.random() * this.#words.length)]
   }
 
-  static wordUrl = 'https://wordle2023.azurewebsites.net/word'
+  //static wordUrl = 'https://wordle2023.azurewebsites.net/word'
 
   static async getWordFromApi(): Promise<string> {
     // Make axios call to get the word from
-    const response = await Axios.get(this.wordUrl)
+    //const response = await Axios.get(this.wordUrl)
 
-    console.log(response.data)
-    return response.data
+    //console.log(response.data)
+    //return response.data
+    return "fucks"
   }
 
   static isValidWord(word: string): boolean {
