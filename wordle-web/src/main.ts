9055856b01c8db738bf3d30e51ed032ea10fe19e
VForev <85714161+VForev@users.noreply.github.com>
2023-05-10 09:06:39 -0700

    Revert "trying nicks data base"
    This reverts commit 6fadae286f5b125729c971f4793f635d61e1ce6d.

--- wordle-web/src/main.ts
+++ wordle-web/src/main.ts
@@ -18,7 +18,7 @@ import { Player } from './scripts/userService'
 if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
   Axios.defaults.baseURL = 'https://localhost:7053/'
 } else {
-  Axios.defaults.baseURL = 'cscd-379-spring-2023-nickb.azurewebsites.net'
+  Axios.defaults.baseURL = 'wordle2023dillon.azurewebsites.net'
 }
 
 Player.SetupAsync()
