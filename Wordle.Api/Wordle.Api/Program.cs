59e4f83d0eedc914558fe0ddc2ec097b3f72ee66
Hunter T <strangeranger01@randomserver.xyz>
2023-05-11 00:57:42 -0700

    Attempt to fix CORS problem

--- Wordle.Api/Wordle.Api/Program.cs
+++ Wordle.Api/Wordle.Api/Program.cs
@@ -13,7 +13,6 @@ builder.Services.AddCors(options =>
                                                                             policy.WithOrigins("*");
                                                                             policy.AllowAnyMethod();
                                                                             policy.AllowAnyHeader();
-                                                                            policy.AllowAnyOrigin();
                                                                         });
                          });
 
