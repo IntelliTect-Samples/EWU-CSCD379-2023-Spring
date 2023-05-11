1d001f517263d2de33bab8d299689c7ec5b11792
Nick Parkman <nparkman@ewu.edu>
2023-05-11 16:48:21 -0700

    fixed

--- wordle-web/src/main.ts
+++ wordle-web/src/main.ts
@@ -18,7 +18,7 @@ import Axios from 'axios'
 if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
   Axios.defaults.baseURL = 'http://localhost:5006/'
 } else {
-  Axios.defaults.baseURL = 'wordle2023-nparkman.azurewebsites.net'
+  Axios.defaults.baseURL = 'https://wordle2023-nparkman.azurewebsites.net'
 }
 
 const vuetify = createVuetify({
