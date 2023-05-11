ae30553cad9df54c86db2949852961c98831103c
Eric Vo <tvo12@ewu.edu>
2023-05-11 04:52:15 -0700

    fixes

--- wordle-web/src/main.ts
+++ wordle-web/src/main.ts
@@ -18,7 +18,7 @@ import Axios from 'axios'
 if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
   Axios.defaults.baseURL = 'http://localhost:5006/'
 } else {
-  Axios.defaults.baseURL = 'cscd-379-spring-2023-nparkman.database.windows.net'
+  Axios.defaults.baseURL = 'wordle2023-nparkman.azurewebsites.net/'
 }
 
 const vuetify = createVuetify({
