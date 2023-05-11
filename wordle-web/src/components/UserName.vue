1f36c045c364bf19024365e4551118f4e36c6d70
TannerStephenson <117472530+TannerStephenson@users.noreply.github.com>
2023-05-10 17:56:14 -0700

    Leaderboard w/ animations. Username fix.

--- wordle-web/src/components/UserName.vue
+++ wordle-web/src/components/UserName.vue
@@ -5,15 +5,20 @@
   </v-btn>
 
   <v-dialog v-model="dialog" transition="dialog-bottom-transition" width="auto" persistent>
-    <v-card class="justify-center text-center" width="500px" height="150px">
+    <v-card class="justify-center text-center" width="520px" height="280px">
+      <v-text-field>Please login or continue as Guest</v-text-field>
       You are currently logged in as: {{ usersName }}
       <v-text-field
         label="Enter a User Name"
         variant="outlined"
         v-model="inputUserName"
         @input="updateUserName"
+        class="mt-4 ml-4 mr-4"
       ></v-text-field>
-      <v-btn variant="outlined" @click="saveUserName" :disabled="!inputUserName">Enter</v-btn>
+      <div class="button-container">
+        <v-btn variant="outlined" @click="saveUserName" :disabled="!inputUserName">Enter</v-btn>
+        <v-btn variant="outlined" @click="cancelUserName">Cancel</v-btn>
+      </div>
     </v-card>
   </v-dialog>
 </template>
@@ -22,7 +27,7 @@
 import { watch } from 'vue'
 import { ref } from 'vue'
 
-const dialog = ref(false)
+const dialog = ref(true)
 const usersName = ref('guest')
 const inputUserName = ref('')
 
@@ -34,6 +39,11 @@ function saveUserName() {
   usersName.value = inputUserName.value
   dialog.value = false
   sendUsername()
+}
+
+function cancelUserName() {
+  inputUserName.value = 'guest'
+  dialog.value = false
 }
 
 const emits = defineEmits<{
@@ -65,3 +75,14 @@ watch(
   }
 )
 </script>
+
+<style scoped>
+.button-container {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+.button-container > * {
+  margin: 0 20px;
+}
+</style>
--- wordle-web/src/views/LeaderView.vue
+++ wordle-web/src/views/LeaderView.vue
@@ -1,19 +1,58 @@
 <template>
-  <h1 align="center">Top 10 Wordle Scores</h1>
-  <v-row class="justify-center" dense v-for="(leader, index) in leaderData" :key="leader.playerId">
-    <v-col cols="auto"
-      >{{ index + 1 }}: {{ leader.name }} - {{ leader.gameCount }} games,
-      {{ leader.averageAttempts }} average attempts</v-col
+  <v-sheet
+    :rounded="true"
+    :elevation="4"
+    width="600px"
+    height="600px"
+    class="mx-auto my-6"
+    color="green"
+  >
+    <v-card height="100px" class="mb-4" :style="{ padding: '7px' }">
+      <v-card-item class="mx-auto mr-12">
+        <v-card-title class="text-center" style="font-size: 3em">
+          <v-icon icon="mdi-trophy" class="mb-5" color="yellow" size="60"> </v-icon>
+          Leaderboard
+        </v-card-title>
+      </v-card-item>
+    </v-card>
+    <v-row
+      class="justify-center"
+      dense
+      v-for="(leader, index) in leaderData"
+      :key="leader.playerId"
+      :class="{ 'flip-in-x': showLeaderData }"
     >
-  </v-row>
+      <v-col cols="auto">
+        <v-card
+          :style="{
+            padding: '7px',
+            width: '475px',
+            animationDelay: `${index * 0.03}s`
+          }"
+          class="flip-in-x"
+        >
+          {{ index + 1 }}: {{ leader.name }} - {{ leader.gameCount }} games,
+          {{ leader.averageAttempts }} average attempts
+          <v-icon v-if="index === 0" color="yellow" size="25">mdi-crown</v-icon>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-sheet>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import Axios from 'axios'
 import type { LeaderData } from '@/types/LeaderData'
 
-const leaderData = ref<LeaderData[]>()
+const leaderData = ref<LeaderData[]>([])
+const showLeaderData = ref(false)
+
+onMounted(() => {
+  setTimeout(() => {
+    showLeaderData.value = true
+  })
+})
 
 Axios.get('/Leaderboard/GetTopTenScores')
   .then((response) => {
@@ -24,3 +63,23 @@ Axios.get('/Leaderboard/GetTopTenScores')
     console.log(err)
   })
 </script>
+
+<style scoped>
+.flip-in-x {
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
+}
+
+@keyframes flip-in-x {
+  0% {
+    transform: rotateX(90deg);
+  }
+  100% {
+    transform: rotateX(0deg);
+  }
+}
+
+.flip-in-x {
+  animation-name: flip-in-x;
+}
+</style>
