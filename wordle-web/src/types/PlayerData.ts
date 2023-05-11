dee1fe484aadfbd04ddb3c97e497706d347b513a
Nick Parkman <nparkman@ewu.edu>
2023-05-10 22:10:04 -0700

    leaderboard edits

--- wordle-web/src/types/PlayerData.ts
+++ wordle-web/src/types/PlayerData.ts
@@ -2,6 +2,8 @@ export interface PlayerData {
     PlayerID: number
     Name: string
     GameCount: number
+    TotalAttempts: number
     AverageAttempts: number
+    TotalSecondsPlayed: number
     AverageSecondsPerGame: number
 }
--- wordle-web/src/views/LeaderboardView.vue
+++ wordle-web/src/views/LeaderboardView.vue
@@ -2,30 +2,14 @@
   <v-sheet>
     <v-card>
       <v-card-title>Leader Board</v-card-title>
+        
+      <template v-for=" player in playerData" :key="player.PlayerID">
         <v-progress-linear 
-          v-model="progress[0]"
-          color="amber"
-          height="25"
-        >
-        <strong>First: {{ playerData[0].Name }} Number of Games: {{ playerData[0].GameCount }} 
-          Average Guesses: {{ playerData[0].AverageAttempts }} Average Time: {{ playerData[0].AverageSecondsPerGame }} </strong> 
-      </v-progress-linear>
-      <v-progress-linear 
-          v-model="progress[1]"
-          color="#CD7F32S"
-          height="25"
-        >
-        <strong>Second: {{ playerData[1].Name }} Number of Games: {{ playerData[1].GameCount }} 
-          Average Guesses: {{ playerData[1].AverageAttempts }} Average Time: {{ playerData[1].AverageSecondsPerGame }} </strong> 
-      </v-progress-linear>
-      <template v-for="i in 8" :key="i.id">
-        <v-progress-linear 
-          v-model="progress[i+3]"
+          v-model="progress[player.PlayerID]"
           color="#757575"
           height="25"
         >
-        <strong>{{ i+3 }}: {{ playerData[i+3].Name }} Number of Games: {{ playerData[i+3].GameCount }} 
-          Average Guesses: {{ playerData[i+3].AverageAttempts }} Average Time: {{ playerData[i+3].AverageSecondsPerGame }}</strong>
+        <strong>{{ player.Name }}</strong>>
         </v-progress-linear>
       </template>
       <v-btn @click="$router.back()">Back</v-btn>
