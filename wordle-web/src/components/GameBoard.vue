0d233c47b9f1329e78477bed42bbbe746c6c2512
alowe4 <117127121+alowe4@users.noreply.github.com>
2023-05-19 16:34:20 -0700

    game reactivity, stuff scales w/ window size

--- wordle-web/src/components/GameBoard.vue
+++ wordle-web/src/components/GameBoard.vue
@@ -1,7 +1,11 @@
 <template>
   <v-row class="justify-center" dense v-for="(word, r) in game.guesses" :key="r">
     <v-col cols="auto" v-for="(letter, c) in word.letters" :key="`${r}-${c}`">
-      <LetterButton :letter="letter" @click="letterClick(letter)" />
+      <LetterButton 
+        :letter="letter" 
+        @click="letterClick(letter)" 
+        :size="xs? 30 : sm? 40 : md? 50 : lg? 60 : xl? 70 : 70"
+      />
     </v-col>
   </v-row>
 </template>
@@ -10,6 +14,12 @@
 import LetterButton from '@/components/LetterButton.vue'
 import type { Letter } from '@/scripts/letter'
 import type { WordleGame } from '@/scripts/wordleGame'
+
+//-----------------reactivity
+import { useDisplay } from 'vuetify'
+
+const { xs, sm, md, lg, xl } = useDisplay()
+//-----------------
 
 defineProps<{
   game: WordleGame
