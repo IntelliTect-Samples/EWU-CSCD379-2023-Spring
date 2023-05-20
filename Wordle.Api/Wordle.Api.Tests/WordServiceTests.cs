f29f967bdcbac84bdcea007cdb61f0b5d1dc4572
Hunter T <strangeranger01@randomserver.xyz>
2023-05-19 17:34:12 -0700

    Revert removed method, and format using clang-format

--- Wordle.Api/Wordle.Api.Tests/WordServiceTests.cs
+++ Wordle.Api/Wordle.Api.Tests/WordServiceTests.cs
@@ -3,36 +3,36 @@ using Wordle.Api.Services;
 
 namespace Wordle.Api.Tests
 {
-    [TestClass]
-    public class WordServiceTests : DatabaseTestBase
+[TestClass]
+public class WordServiceTests : DatabaseTestBase
+{
+    [TestMethod]
+    public async Task GetWord()
     {
-        [TestMethod]
-        public async Task GetWord()
-        {
-            using var context = new AppDbContext(Options);
-            var service = new WordService(context);
-            Seeder.Seed(context);
+        using var context = new AppDbContext(Options);
+        var service = new WordService(context);
+        Seeder.Seed(context);
 
-            var word = await service.GetRandomWord();
+        var word = await service.GetRandomWord();
 
-            Assert.IsNotNull(word);
-            Assert.AreEqual(5, word.Text.Length);
-        }
-
-        [TestMethod]
-        public async Task GetWordOfTheDay()
-        {
-            using var context = new AppDbContext(Options);
-            var service = new WordService(context);
-            Seeder.Seed(context);
-
-            var date = DateTime.UtcNow;
-            
-            var word = await service.GetWordOfTheDay(TimeSpan.FromHours(-8), date);
-            
-            var word2 = await service.GetWordOfTheDay(TimeSpan.FromHours(-8), date);
-
-            Assert.AreEqual(word, word2);
-        }
+        Assert.IsNotNull(word);
+        Assert.AreEqual(5, word.Text.Length);
     }
+
+    [TestMethod]
+    public async Task GetWordOfTheDay()
+    {
+        using var context = new AppDbContext(Options);
+        var service = new WordService(context);
+        Seeder.Seed(context);
+
+        var date = DateTime.UtcNow;
+
+        var word = await service.GetWordOfTheDay(TimeSpan.FromHours(-8), date);
+
+        var word2 = await service.GetWordOfTheDay(TimeSpan.FromHours(-8), date);
+
+        Assert.AreEqual(word, word2);
+    }
+}
 }
