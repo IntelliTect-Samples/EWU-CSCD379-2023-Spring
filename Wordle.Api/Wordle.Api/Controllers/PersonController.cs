e957b503151660f2643cc735b76033ec0f481d33
BenFristadSchool <bfristad@ewu.edu>
2023-05-21 09:40:45 -0700

    deploy Assigment-4 Backend

--- Wordle.Api/Wordle.Api/Controllers/PersonController.cs
+++ Wordle.Api/Wordle.Api/Controllers/PersonController.cs
@@ -20,7 +20,6 @@ public class PlayerController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<PlayerDto>> Get(Guid playerId)
     {
-        // hi
         Player? player = await _PlayerService.GetAsync(playerId);
 
         if (player is not null)
