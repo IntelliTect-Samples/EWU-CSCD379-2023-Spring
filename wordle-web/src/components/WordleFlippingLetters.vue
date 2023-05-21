8cfcf73e08bd4a65bfa8ec81860d61f51f3e5acf
Grant Erickson <grant@granterickson.com>
2023-05-21 06:04:42 -0700

    Improved splash

--- wordle-web/src/components/WordleFlippingLetters.vue
+++ wordle-web/src/components/WordleFlippingLetters.vue
@@ -1,31 +1,53 @@
 <template>
   <div>
-    <div class="square num1" style="animation-delay: 0s">W</div>
-    <div class="square num2" style="animation-delay: 0.1s">O</div>
-    <div class="square num3" style="animation-delay: 0.2s">R</div>
-    <div class="square num4" style="animation-delay: 0.3s">D</div>
-    <div class="square num5" style="animation-delay: 0.4s">L</div>
-    <div class="square num6" style="animation-delay: 0.5s">E</div>
+    <div v-for="(letter, index) in letters" :class="letter.classes" :key="index">
+      {{ letter.letter }}
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { onBeforeUnmount } from 'vue'
+import { onBeforeUnmount, reactive } from 'vue'
 import { onMounted } from 'vue'
 
-onMounted(() => {
-  for (let i = 1; i <= 6; i++) {
-    setTimeout(() => {
-      document.querySelector(`.num${i}`)!.classList.add('square2')
-    }, i * 100)
+// Represents the letters
+class Letter {
+  constructor(public letter: string) {}
+  classList = ['square']
+  get classes() {
+    return this.classList.join(' ')
   }
+}
 
-  console.log('mounted')
+const letters = reactive([
+  new Letter('W'),
+  new Letter('O'),
+  new Letter('R'),
+  new Letter('D'),
+  new Letter('L'),
+  new Letter('E')
+])
+
+onMounted(() => {
+  for (const [index, letter] of letters.entries()) {
+    setTimeout(() => {
+      letter.classList.push('square2')
+      if (Math.random() > 0.03) {
+        letter.classList.push('green')
+      } else {
+        letter.classList.push('yellow')
+        setTimeout(() => {
+          letter.classList.pop()
+          letter.classList.push('green-switch')
+        }, 1300)
+      }
+    }, index * 100)
+  }
 })
 
 onBeforeUnmount(() => {
-  for (let i = 1; i <= 6; i++) {
-    document.querySelector(`.num${i}`)!.classList.remove('square2')
+  for (const letter of letters) {
+    letter.classList.pop()
   }
 })
 </script>
@@ -66,17 +88,29 @@ div {
   margin: 0px 0px;
   border-radius: 0px;
   opacity: 0.5;
+  font-size: 2em;
 }
 
 .square2 {
-  background-color: green;
   transform: rotateY(0deg);
-  border: 1px solid darkgreen;
   margin: 0px 3px;
   color: white;
-  border-radius: 5px;
+  border-radius: 15px;
   opacity: 1;
-  transition: background-color 2s ease-out, transform 1s ease-out, boarder 1s ease-out,
-    margin 0.5s ease-out, color 1s ease-out, border-radius 2s linear, opacity 1s linear;
+  transition: transform 1s ease-out, margin 0.5s ease-out, color 1s ease-out,
+    border-radius 1s linear, opacity 1s linear, background-color 2s ease-out;
+}
+
+.green {
+  background-color: green;
+}
+
+.yellow {
+  background-color: yellow;
+}
+
+.green-switch {
+  background-color: green;
+  transition: background-color 0.2s linear;
 }
 </style>
