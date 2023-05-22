2e6c44757797930760d9fd4feef4ad7e006ef9ef
Cayden Kappes <caydenkappes@gmail.com>
2023-05-22 12:17:25 -0700

    change api to connect to

--- wordle-web/src/main.ts
+++ wordle-web/src/main.ts
@@ -17,7 +17,7 @@ import Axios from 'axios'
 if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
   Axios.defaults.baseURL = 'https://localhost:7053/'
 } else {
-  Axios.defaults.baseURL = 'https://ckappeswordleclone.azurewebsites.net/'
+  Axios.defaults.baseURL = 'https://ckappeswordlecloneassignment4app.azurewebsites.net/'
 }
 
 const vuetify = createVuetify({
