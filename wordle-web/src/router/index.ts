4cac1bba3f5285ed81e5a360645ba62f368241c5
roydunn19 <roydunn100@gmail.com>
2023-05-22 14:54:46 -0700

    stuff

--- wordle-web/src/router/index.ts
+++ wordle-web/src/router/index.ts
@@ -3,12 +3,18 @@ import WordleView from '../views/WordleView.vue'
 import AboutView from '../views/AboutView.vue'
 import LeaderboardView from '../views/LeaderboardView.vue'
 import InstructionsViewVue from '@/views/InstructionsView.vue'
+import HomeViewVue from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'home',
+      component: HomeViewVue
+    },
+    {
+      path: '/wordle',
       name: 'wordle',
       component: WordleView
     },
---
+++ wordle-web/src/views/HomeView.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-container class="fill-height" fluid>
+    <v-row justify="center" align="center">
+      <v-col cols="12" align="center">
+        <v-icon icon="mdi-alpha-w-box-outline" :size="150" class="text-shadow"></v-icon>
+      </v-col>
+      <v-col cols="12" align="center">
+        <h1 class="text-h1 text-shadow">Wordle</h1>
+      </v-col>
+      <v-col cols="12" md="6" xl="3" align="center">
+        <h1 class="text-h2 text-shadow">Get 6 chances to guess a 5 letter word.</h1>
+      </v-col>
+      <v-col cols="12" align="center">
+        <v-btn
+          variant="outlined"
+          class="text-h6 rounded-xl elevation-0 text-shadow button-theme"
+          to="/wordle"
+        >
+          Worlde
+        </v-btn>
+        <v-divider vertical :thickness="20" v-if="!display.xs"></v-divider>
+        <v-col cols="12" v-if="display.xs"></v-col>
+        <v-btn
+          variant="outlined"
+          class="text-h6 rounded-xl elevation-0 text-shadow button-theme"
+          to="/play"
+        >
+          Playground
+        </v-btn>
+        <v-divider vertical :thickness="20" v-if="!display.xs"></v-divider>
+        <v-col cols="12" v-if="display.xs"></v-col>
+        <v-btn
+          variant="outlined"
+          class="text-h6 rounded-xl elevation-0 text-shadow button-theme"
+          to="/leaderboard"
+        >
+          Leaderboard
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { Services } from '@/scripts/services'
+import { inject, reactive } from 'vue'
+import { useDisplay } from 'vuetify/lib/framework.mjs'
+// Add this to make testing work because useDisplay() throws an error when testing
+// Wrap useDisplay in a function so that it doesn't get called during testing.
+const display = inject(Services.Display, () => reactive(useDisplay())) as unknown as ReturnType<
+  typeof useDisplay
+>
+</script>
+
+<style scoped>
+.button-theme {
+  color: white;
+  border: 2px solid white;
+  min-width: 155px;
+  min-height: 50px;
+}
+</style>
