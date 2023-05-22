4aebde63e2eab8781886a5b851a936cb73dc0921
Cayden Kappes <caydenkappes@gmail.com>
2023-05-22 12:18:47 -0700

    format

--- wordle-web/src/views/HowToPlay.vue
+++ wordle-web/src/views/HowToPlay.vue
@@ -3,12 +3,11 @@
     <v-card>
       <div class="bg-primary text-h5 pa-3 text-center">How To Play</div>
       <v-card-text>
-        <div v-for="(index) in 5" :key="index" >
+        <div v-for="index in 5" :key="index">
           <div class="d-flex justify-start bg-secondary text-h6">{{ index }}</div>
           <v-card-text class="d-flex flex-column align-center">
             <v-row dense>
               <v-col cols="12" class="text-end">Total Games -</v-col>
-              
             </v-row>
           </v-card-text>
         </div>
