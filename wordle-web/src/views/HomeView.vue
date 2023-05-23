99ce2a1ebd4d1e5b209fac4db5a8b0fc6c6ad9cc
Nolan Posey <nposey1@ewu.edu>
2023-05-22 17:23:27 -0700

    Improved the landing page.

--- wordle-web/src/views/HomeView.vue
+++ wordle-web/src/views/HomeView.vue
@@ -1,14 +1,50 @@
 <template>
-  <v-card class="align-center">
-    <v-card-title>Welcome to Wordle!</v-card-title>
-    <v-card-text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-      labore et dolore magna aliqua. Odio ut sem nulla pharetra diam sit amet. Porta non pulvinar
-      neque laoreet suspendisse interdum consectetur libero id. Ullamcorper a lacus vestibulum sed
-      arcu. Urna condimentum mattis pellentesque id nibh tortor.
-    </v-card-text>
-    <v-card-actions>
-      <v-btn class="align-center" to="/leaderboard">To The Leaderboard</v-btn>
-    </v-card-actions>
-  </v-card>
+  <v-responsive min-width="300px">
+    <v-card>
+      <v-card-title class="justify-center">Welcome To The Good Word</v-card-title>
+      <v-card-item>
+        <v-card-text>
+          In this casual game mode you can practice your skills or just play for fun! 
+        </v-card-text>
+
+        <v-card-actions class="justify-center">
+          <v-btn to="/wordle" rounded="xl" elevation="16">Free Play</v-btn>
+        </v-card-actions>
+      </v-card-item>
+
+      <v-card-item>
+        <v-card-text>
+           Log on daily to play our word of the day in this game mode!
+        </v-card-text>
+
+        <v-card-actions class="justify-center">
+          <v-btn rounded="xl" elevation="16">Daily Word</v-btn>
+        </v-card-actions>
+      </v-card-item>
+
+      <v-card-item>
+        <v-card-text>
+           View the current leaderboard or your personal stats here!
+        </v-card-text>
+
+        <v-card-actions class="justify-center">
+          <v-btn to="/leaderboard" rounded="xl" elevation="16">Leaderboard</v-btn>
+        </v-card-actions>
+      </v-card-item>
+
+      <v-card-item>
+        <v-card-text>
+           First time player? Or just need a refresher? Click here to view the rules of the game!
+        </v-card-text>
+
+        <v-card-actions class="justify-center">
+          <v-btn to="/instructions" rounded="xl" elevation="16">How To Play</v-btn>
+        </v-card-actions>
+      </v-card-item>
+    </v-card>
+  </v-responsive>
 </template>
+
+<script setup lang="ts">
+
+</script>
