7ee8202b67df745e5b35c0178509cde2f360d9b8
Seth Lanante <73133434+slanante1997@users.noreply.github.com>
2023-05-22 20:06:28 -0700

    Update InstructionsView.vue

--- wordle-web/src/views/InstructionsView.vue
+++ wordle-web/src/views/InstructionsView.vue
@@ -64,6 +64,10 @@
 </template>
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+
+const index = ref(0)
+const letter = ref('A')
 
 const router = useRouter()
 
