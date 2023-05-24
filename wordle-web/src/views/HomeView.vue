2bbe98ec7bc25608fd8c2e176aec41f5c0bad820
FenrisStargazer <80987915+FenrisStargazer@users.noreply.github.com>
2023-05-24 14:17:33 -0700

    Literally change almost nothing??
    So prettier stops whining

--- wordle-web/src/views/HomeView.vue
+++ wordle-web/src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <template>
-    <v-parallax height="650" src="https://source.unsplash.com/RwHv7LgeC7s">
-        <div class="d-flex flex-column fill-height justify-center align-center text-white">
-            <h1 class="text-h4 font-weight-thin mb-4">Vuetify</h1>
-            <h4 class="subheading">Build your application today!</h4>
-        </div>
-    </v-parallax>
+  <v-parallax height="650" src="https://source.unsplash.com/RwHv7LgeC7s">
+    <div class="d-flex flex-column fill-height justify-center align-center text-white">
+      <h1 class="text-h4 font-weight-thin mb-4">Vuetify</h1>
+      <h4 class="subheading">Build your application today!</h4>
+    </div>
+  </v-parallax>
 </template>
-<script lang="ts">
-
-</script>
+<script lang="ts"></script>
