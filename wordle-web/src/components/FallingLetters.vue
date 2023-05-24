421252f1ac4441ee1be4f9aeb6d8fe6e331de01e
AntonioGomez12 <103075701+AntonioGomez12@users.noreply.github.com>
2023-05-23 23:31:09 -0700

    remade to ts

--- wordle-web/src/components/FallingLetters.vue
+++ wordle-web/src/components/FallingLetters.vue
@@ -7,53 +7,50 @@
     </div>
   </template>
   
-  <script>
-  export default {
-    data() {
-      return {
-        letters: [],
+  <script setup lang="ts">
+  import { ref, onMounted } from 'vue';
+  
+  const letters = ref<Array<{ id: number; text: string; style: Record<string, string> }>>([]);
+  
+  onMounted(() => {
+    createLetters();
+  });
+  
+  function createLetters() {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  
+    setInterval(() => {
+      const randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+  
+      const letter = {
+        id: Date.now(),
+        text: randomLetter,
+        style: {
+          left: `${Math.random() * 369}%`,
+          top: '-10vh',
+          transform: `
+            translate3d(${Math.random() * 360}px, -10vh, ${Math.random() * -500}px)
+            rotateX(${Math.random() * 360}deg)
+            rotateY(${Math.random() * 360}deg)
+            rotateZ(${Math.random() * 360}deg)
+          `,
+        },
       };
-    },
-    mounted() {
-      this.createLetters();
-    },
-    methods: {
-      createLetters() {
-        const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   
-        setInterval(() => {
-          const randomLetter = letters[Math.floor(Math.random() * letters.length)];
+      letters.value.push(letter);
   
-          const letter = {
-            id: Date.now(),
-            text: randomLetter,
-            style: {
-              left: Math.random() * 369 + '%',
-              top: '-10vh',
-              transform: `
-                translate3d(${Math.random() * 100}px, -10vh, ${Math.random() * -500}px)
-                rotateX(${Math.random() * 360}deg)
-                rotateY(${Math.random() * 360}deg)
-                rotateZ(${Math.random() * 360}deg)
-              `,
-            },
-          };
+      setTimeout(() => {
+        removeLetter(letter.id);
+      }, 8000);
+    }, 400);
+  }
   
-          this.letters.push(letter);
-  
-          setTimeout(() => {
-            this.removeLetter(letter.id);
-          }, 8000);
-        }, 400);
-      },
-      removeLetter(id) {
-        const index = this.letters.findIndex((letter) => letter.id === id);
-        if (index !== -1) {
-          this.letters.splice(index, 1);
-        }
-      },
-    },
-  };
+  function removeLetter(id: number) {
+    const index = letters.value.findIndex((letter) => letter.id === id);
+    if (index !== -1) {
+      letters.value.splice(index, 1);
+    }
+  }
   </script>
   
   <style scoped>
@@ -65,14 +62,14 @@
     height: 100%;
     overflow: hidden;
     z-index: -1;
-    perspective: 1000px; /* Adjust the perspective value as needed */
+    perspective: 3rem; /* Adjust the perspective value as needed */
   }
   
   .falling-letter {
     position: absolute;
     color: #00FF00;
     font-size: 5rem;
-    animation: cascade 8s infinite;
+    animation: cascade 15s infinite;
     transform-style: preserve-3d;
   }
   
