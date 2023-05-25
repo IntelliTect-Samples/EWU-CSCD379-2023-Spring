5a12203fb4eb7c6cd1ab9ec967b4d6d627fde1d5
schemcharger <ngraham7@ewu.edu>
2023-05-25 13:56:42 -0700

    issue test

--- wordle-web/src/router/index.ts
+++ wordle-web/src/router/index.ts
@@ -5,18 +5,18 @@ import LeaderboardView from '../views/LeaderboardView.vue'
 import DailyView from '../views/DailyView.vue'
 import InstructionView from '../views/InstructionView.vue'
 import PreviousView from '../views/PreviousView.vue'
-import IndexView from '../views/IndexView.vue'
+//import IndexView from '../views/IndexView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
+    /*{
       path: '/',
       name: 'index',
       component: IndexView
-    },
+    },*/
     {
-      path: '/game',
+      path: '/',
       name: 'wordle',
       component: WordleView
     },
