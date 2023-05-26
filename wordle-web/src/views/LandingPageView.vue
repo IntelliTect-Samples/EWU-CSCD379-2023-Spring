550997ca9ef5004c4825748c35289f967f65055b
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2023-05-25 23:48:31 -0700

    final edits

--- wordle-web/src/views/LandingPageView.vue
+++ wordle-web/src/views/LandingPageView.vue
@@ -1,46 +1,44 @@
 <template>
   <v-container>
     <div class="d-flex justify-center">Welcome!</div>
-
     <v-card
-    class="text-center"
+      class="text-center"
       title="Try a random word!"
       text="This button will take you to a random wordle. You can play as many as you want, and your stats will be tracked."
       variant="outlined"
     >
       <v-card-actions class="justify-center">
         <router-link to="/wordle">
-          <v-btn> Click Me </v-btn>
+          <v-btn> Wordle </v-btn>
         </router-link>
       </v-card-actions>
     </v-card>
 
     <v-card
-    class="text-center"
+      class="text-center"
       title="Or try the daily word!"
       text="This button will take you to the daily wordle. You can play as many as you want, and your stats will be tracked."
       variant="outlined"
     >
       <v-card-actions class="justify-center">
         <router-link to="/wordoftheday">
-          <v-btn> Click Me </v-btn>
+          <v-btn> Daily Word </v-btn>
         </router-link>
       </v-card-actions>
     </v-card>
 
     <v-card
-    class="text-center"
+      class="text-center"
       title="Read the instructions"
       text="This button will take you to the instructions page. You can read about how to play the game."
       variant="outlined"
     >
       <v-card-actions class="justify-center">
         <router-link to="/instructions">
-          <v-btn> Click Me </v-btn>
+          <v-btn> How To Play</v-btn>
         </router-link>
       </v-card-actions>
     </v-card>
-
   </v-container>
 </template>
 <script lang="ts" setup>
