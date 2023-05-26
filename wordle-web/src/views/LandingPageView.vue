dff4c1d00306ed44cbdac5c4fd9d85205472b6f6
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2023-05-25 23:46:29 -0700

    center buttons

--- wordle-web/src/views/LandingPageView.vue
+++ wordle-web/src/views/LandingPageView.vue
@@ -1,13 +1,14 @@
 <template>
   <v-container>
-    <!-- <div class="d-flex justify-center">Welcome!</div> -->
+    <div class="d-flex justify-center">Welcome!</div>
 
-    <!-- <v-card
+    <v-card
+    class="text-center"
       title="Try a random word!"
       text="This button will take you to a random wordle. You can play as many as you want, and your stats will be tracked."
       variant="outlined"
     >
-      <v-card-actions>
+      <v-card-actions class="justify-center">
         <router-link to="/wordle">
           <v-btn> Click Me </v-btn>
         </router-link>
@@ -15,11 +16,12 @@
     </v-card>
 
     <v-card
+    class="text-center"
       title="Or try the daily word!"
       text="This button will take you to the daily wordle. You can play as many as you want, and your stats will be tracked."
       variant="outlined"
     >
-      <v-card-actions>
+      <v-card-actions class="justify-center">
         <router-link to="/wordoftheday">
           <v-btn> Click Me </v-btn>
         </router-link>
@@ -27,33 +29,14 @@
     </v-card>
 
     <v-card
+    class="text-center"
       title="Read the instructions"
       text="This button will take you to the instructions page. You can read about how to play the game."
       variant="outlined"
     >
-      <v-card-actions>
+      <v-card-actions class="justify-center">
         <router-link to="/instructions">
           <v-btn> Click Me </v-btn>
-        </router-link>
-      </v-card-actions>
-    </v-card> -->
-
-    <v-card
-      title="Welcome!"
-      text="We are so glad you could make it! This is a word game called Wordle. You can play a random wordle, or you can play the daily wordle. You can play as many as you want, and your stats will be tracked."
-      variant="outlined"
-    >
-      <v-card-actions>
-        <router-link to="/instructions">
-          <v-btn> How To Play </v-btn>
-        </router-link>
-
-          <router-link to="/wordoftheday">
-          <v-btn> Play Daily Word </v-btn>
-        </router-link>
-
-          <router-link to="/wordle">
-          <v-btn> Play Random Word</v-btn>
         </router-link>
       </v-card-actions>
     </v-card>
