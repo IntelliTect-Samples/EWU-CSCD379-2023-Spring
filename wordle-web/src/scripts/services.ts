4801fb5b0bb881eb73a98d973a70bb033728795f
Nick Parkman <nparkman@ewu.edu>
2023-05-25 21:29:14 -0700

    Auto stash before merge of "Assign-4" and "origin/Assign-4"

--- wordle-web/src/scripts/services.ts
+++ wordle-web/src/scripts/services.ts
@@ -1,4 +1,10 @@
+<<<<<<< Updated upstream
 export class Services {
     static readonly Display = 'Display'
     static readonly PlayerService = 'PlayerService'
+=======
+export class Services {
+    static readonly Display = 'Display'
+    static readonly PlayerService = 'PlayerService'
+>>>>>>> Stashed changes
   }
