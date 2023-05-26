eb3f17a811bb74d33819c55b138ae1b80dbaf6ea
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2023-05-25 23:37:07 -0700

    landing page

--- wordle-web/src/views/LandingPageView.vue
+++ wordle-web/src/views/LandingPageView.vue
@@ -1,43 +1,30 @@
 <template>
-    <v-container>
-      <div class="d-flex justify-center bg-secondary text-h6" style="margin: 5%">Wordle</div>
-  
-      <v-row style="text-align: center; margin: 16px">
-        <v-col>
-          <RouterLink to="/wordle" class="d-flex justify-center">
-            <v-card style="min-width: 350px; margin: 16px">
-              <v-card-title>Random Wordle</v-card-title>
-              <v-row style="margin: 8px">
-                <v-card-text style="padding: 8px">- Random Wordles</v-card-text>
-              </v-row>
-              <v-row style="margin: 8px">
-                <v-card-text style="padding: 8px">- Increase Your Stats</v-card-text>
-              </v-row>
-              <v-row style="margin: 8px">
-                <v-card-text style="padding: 8px">- Mostly For Fun</v-card-text>
-              </v-row>
-            </v-card>
-          </RouterLink>
-        </v-col>
-        <v-col>
-          <RouterLink to="/wordoftheday" class="d-flex justify-center">
-            <v-card style="min-width: 350px; margin: 16px">
-              <v-card-title>Daily Wordle</v-card-title>
-              <v-row style="margin: 8px">
-                <v-card-text style="padding: 8px">- One Wordle Per Day</v-card-text>
-              </v-row>
-              <v-row style="margin: 8px">
-                <v-card-text style="padding: 8px">- View Daily Data</v-card-text>
-              </v-row>
-              <v-row style="margin: 8px">
-                <v-card-text style="padding: 8px">- Also Mostly For Fun</v-card-text>
-              </v-row>
-            </v-card>
-          </RouterLink>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
-  <script lang="ts" setup>
-  import { RouterLink } from 'vue-router'
-  </script>
+  <v-container>
+    <div class="d-flex justify-center">Welcome!</div>
+    <v-card
+      title="Try a random word!"
+      text="This button will take you to a random wordle. You can play as many as you want, and your stats will be tracked."
+      variant="outlined"
+    >
+      <v-card-actions>
+        <router-link to="/wordle">
+          <v-btn> Click Me </v-btn>
+        </router-link>
+      </v-card-actions>
+    </v-card>
+    <v-card
+      title="Or try the daily word!"
+      text="This button will take you to the daily wordle. You can play as many as you want, and your stats will be tracked."
+      variant="outlined"
+    >
+      <v-card-actions>
+        <router-link to="/wordoftheday">
+          <v-btn> Click Me </v-btn>
+        </router-link>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router'
+</script>
