1db78e350614e6ffd89a2ba58de2b7518d105788
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2023-05-25 23:42:10 -0700

    landing page
    landing page improvements

--- wordle-web/src/views/LandingPageView.vue
+++ wordle-web/src/views/LandingPageView.vue
@@ -1,7 +1,8 @@
 <template>
   <v-container>
-    <div class="d-flex justify-center">Welcome!</div>
-    <v-card
+    <!-- <div class="d-flex justify-center">Welcome!</div> -->
+
+    <!-- <v-card
       title="Try a random word!"
       text="This button will take you to a random wordle. You can play as many as you want, and your stats will be tracked."
       variant="outlined"
@@ -12,6 +13,7 @@
         </router-link>
       </v-card-actions>
     </v-card>
+
     <v-card
       title="Or try the daily word!"
       text="This button will take you to the daily wordle. You can play as many as you want, and your stats will be tracked."
@@ -23,6 +25,39 @@
         </router-link>
       </v-card-actions>
     </v-card>
+
+    <v-card
+      title="Read the instructions"
+      text="This button will take you to the instructions page. You can read about how to play the game."
+      variant="outlined"
+    >
+      <v-card-actions>
+        <router-link to="/instructions">
+          <v-btn> Click Me </v-btn>
+        </router-link>
+      </v-card-actions>
+    </v-card> -->
+
+    <v-card
+      title="Welcome!"
+      text="We are so glad you could make it! This is a word game called Wordle. You can play a random wordle, or you can play the daily wordle. You can play as many as you want, and your stats will be tracked."
+      variant="outlined"
+    >
+      <v-card-actions>
+        <router-link to="/instructions">
+          <v-btn> How To Play </v-btn>
+        </router-link>
+
+          <router-link to="/wordoftheday">
+          <v-btn> Play Daily Word </v-btn>
+        </router-link>
+
+          <router-link to="/wordle">
+          <v-btn> Play Random Word</v-btn>
+        </router-link>
+      </v-card-actions>
+    </v-card>
+
   </v-container>
 </template>
 <script lang="ts" setup>
