ecbb4cad8ae7928006cb48e43434619650e4b8c0
BenFristadSchool <bfristad@ewu.edu>
2023-06-01 19:00:22 -0700

    removed side nav

--- client/src/components/NoteEditor.vue
+++ client/src/components/NoteEditor.vue
@@ -27,31 +27,9 @@
             </div>
           </div>
         </v-card>
-        <v-layout>
-          <v-navigation-drawer expand-on-hover rail>
-            <v-list>
-              <v-list-item
-                prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-                title="Sandra Adams"
-                subtitle="sandra_a88@gmailcom"
-              ></v-list-item>
-            </v-list>
-
-            <v-divider></v-divider>
-
-            <v-list density="compact" nav>
-              <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
-              <v-list-item
-                prepend-icon="mdi-account-multiple"
-                title="Shared with me"
-                value="shared"
-              ></v-list-item>
-              <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
-            </v-list>
-          </v-navigation-drawer>
-        </v-layout>
-      </v-col> </v-row
-  ></v-container>
+      </v-col>
+    </v-row></v-container
+  >
 </template>
 
 <script setup lang="ts">
