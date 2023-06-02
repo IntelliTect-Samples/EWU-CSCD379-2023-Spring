323f5f655b71eac9cf2d055b860f057f8b906243
Neriah Graham <ngraham7@ewu.edu>
2023-06-02 16:23:13 -0700

    View additions

--- wordle-web/src/views/WordEditorView.vue
+++ wordle-web/src/views/WordEditorView.vue
@@ -1,27 +1,35 @@
 <template>
-  <v-row v-for="word in words" :key="word.text" class="border my-3">
-    <v-col cols="6" class="text-end">{{ word.text }}</v-col>
-    <v-col cols="6" class="text-end"><v-checkbox v-bind="word.isCommon" /></v-col>
-  </v-row>
+  <v-card class="w-75">
+    <v-row class="border my-3">
+      <v-col>Word</v-col>
+      <v-col>Is Common</v-col>
+    </v-row>
+    <v-row v-for="word in words" :key="word.text" class="border my-3">
+      <v-col >{{ word.text }}</v-col>
+      <v-col><v-checkbox v-model="word.isCommon"/></v-col>
+    </v-row>
+  </v-card>
 </template>
 
 <script setup lang="ts">
 import Axios from 'axios'
 
 import { ListWord } from '@/scripts/listWord'
+import { ref } from 'vue'
 
-const words = [
+const words = ref([
   new ListWord('alpha', true),
   new ListWord('bravo', false),
   new ListWord('charlie', true),
   new ListWord('delta', true)
-]
-/*axios.get('GetManyWords', {
+])
+
+Axios.get('GetManyWords', {
   params: {
     Count: 45
   }
 }).then((result) => {
   console.log(result.data)
-  words = result.data as listWord[]
-})*/
+  words.value = result.data as ListWord[]
+})
 </script>
