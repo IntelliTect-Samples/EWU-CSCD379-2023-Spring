230dbd3c8b852a789959a6b398569f78943d8470
BenFristadSchool <bfristad@ewu.edu>
2023-06-03 10:54:27 -0700

    added word editor view

---
+++ wordle-web/src/components/AddWord.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card height="200px">
+    <v-card-text>
+      Add Word:
+      <v-text-field
+        @input="editText($event.target.value)"
+        variant="outlined"
+        style="display: flex; flex-direction: column; flex-grow: 1; width: 500px"
+      />
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn @click="addWord()">Add Word</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const input = ref<string>()
+
+const editText = (text: string) => {
+  input.value = text
+}
+
+const addWord = () => {
+  console.log(input.value)
+}
+</script>
---
+++ wordle-web/src/views/WordEditorView.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container
+    ><v-row justify="center">
+      <v-col cols="12" style="display: flex; justify-content: center">
+        <div style="display: flex; align-items: center; justify-content: center; flex-wrap: wrap">
+          <h3 class="text-h3">Word Editor</h3>
+        </div>
+      </v-col>
+      <v-col cols="12" style="display: flex; justify-content: center">
+        <v-card
+          style="
+            padding: 10px;
+            display: flex;
+            flex-direction: column;
+            height: 600px;
+            width: fit-content;
+          "
+        >
+          <DeleteWord />
+          <AddWord />
+          <CommonWord />
+        </v-card>
+      </v-col> </v-row
+  ></v-container>
+</template>
+
+<script setup lang="ts">
+import DeleteWord from '../components/DeleteWord.vue'
+import AddWord from '../components/AddWord.vue'
+import CommonWord from '../components/CommonWord.vue'
+</script>
