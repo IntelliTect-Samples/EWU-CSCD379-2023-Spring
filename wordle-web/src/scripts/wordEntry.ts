734d4f329c3f76f2a3c23dbe73c6c6879486e374
VeraKorchemnaya <vkorchemnaya@ewu.edu>
2023-06-03 16:46:38 -0700

    Create table to view words. User selected pagination.

---
+++ wordle-web/src/scripts/wordEntry.ts
@@ -0,0 +1,6 @@
+export class WordEntry {
+  wordId: number = 0
+  text: string = ''
+  isCommon: boolean = false
+  isUsed: boolean = false
+}
--- wordle-web/src/views/WordEditorView.vue
+++ wordle-web/src/views/WordEditorView.vue
@@ -1,5 +1,71 @@
 <template>
-  <h1>WordEditorView</h1>
+  <v-table>
+    <thead>
+      <tr class="align-center">
+        <th class="text-left" colspan="5"><v-text-field label="Search"></v-text-field></th>
+      </tr>
+    </thead>
+    <thead>
+      <tr class="align-center">
+        <th class="text-center">Index</th>
+        <th class="text-center">Word</th>
+        <th class="text-center">Well Known?</th>
+        <th class="text-center">Save</th>
+        <th class="text-center">Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(word, index) in wordList" :key="word.wordId">
+        <td class="text-center">{{ index + 1 }}</td>
+        <td class="text-center">{{ word.text }}</td>
+
+        <td class="align-center"><v-checkbox :model-value="word.isCommon"></v-checkbox></td>
+
+        <td>
+          <v-btn variant="outlined"> Save </v-btn>
+        </td>
+
+        <td class="align-center"><v-btn variant="outlined"> Delete </v-btn></td>
+      </tr>
+      <tr>
+        <td class="text-center" colspan="2">Words per page:</td>
+        <td class="text-center" colspan="2">
+          <v-select
+            v-model="entriesPerPage"
+            :items="[10, 25, 50, 100]"
+            label="Words per page"
+          ></v-select>
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
 
-<script setup lang="ts"></script>
+<!-- How can I make content centered? -->
+<!-- 
+ <td style="display: flex; justify-content: center; align-items: center">
+          <v-btn variant="outlined"> Save </v-btn>
+        </td> -->
+
+<!-- class="d-flex  text-center align-center justify-center " -->
+
+<script lang="ts" setup>
+import Axios from 'axios'
+import { ref } from 'vue'
+import type { WordEntry } from '../scripts/wordEntry'
+import { watch } from 'vue'
+
+const wordList = ref<WordEntry[]>([])
+let entriesPerPage = ref(10)
+
+watch(entriesPerPage, getWords)
+
+getWords()
+
+function getWords() {
+  Axios.get(`/Word/SearchForWord?entriesPerPage=${entriesPerPage.value}`).then((result) => {
+    console.log(result.data)
+    wordList.value = result.data as WordEntry[]
+  })
+}
+</script>
