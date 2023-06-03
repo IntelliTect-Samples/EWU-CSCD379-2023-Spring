2a92137abdb1f514060ecc2810a1af9bc50973ce
alowe4 <117127121+alowe4@users.noreply.github.com>
2023-06-03 10:15:57 -0700

    Axios commands first draft added to Word Editor

--- wordle-web/src/components/WordEditor.vue
+++ wordle-web/src/components/WordEditor.vue
@@ -1,64 +1,36 @@
 <template>
   <div class="text-center">
-    <v-btn
-      color="primary"
-    >
+    <v-btn color="primary">
       Edit Dictionary
 
-      <v-dialog
-        v-model="dialog"
-        activator="parent"
-        width="auto"
-      >
+      <v-dialog v-model="dialog" activator="parent" width="auto">
         <v-card>
-          <v-card-text>
-            Word Editor
-          </v-card-text>
+          <v-card-text> Word Editor </v-card-text>
           <v-card-actions>
             <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
           </v-card-actions>
         </v-card>
         <v-sheet width="300" class="mx-auto">
+          <v-form ref="form">
+            <v-text-field
+              v-model="input"
+              :counter="10"
+              :rules="wordRules"
+              label="Name"
+              required
+            ></v-text-field>
 
-<v-form ref="form">
-  <v-text-field
-    v-model="input"
-    :counter="10"
-    :rules="wordRules"
-    label="Name"
-    required
-  ></v-text-field>
+            <div class="d-flex flex-column">
+              <v-btn color="success" class="mt-4" block @click="addWord"> Add </v-btn>
 
-  <div class="d-flex flex-column">
-    <v-btn
-      color="success"
-      class="mt-4"
-      block
-      @click="addWord"
-    >
-      Add
-    </v-btn>
+              <v-btn color="error" class="mt-4" block @click="flipIsCommon">
+                Flip Is Common Value
+              </v-btn>
 
-    <v-btn
-      color="error"
-      class="mt-4"
-      block
-      @click="flipIsCommon"
-    >
-      Flip Is Common Value
-    </v-btn>
-
-    <v-btn
-      color="warning"
-      class="mt-4"
-      block
-      @click="removeWord"
-    >
-      Remove Word
-    </v-btn>
-  </div>
-</v-form>
-</v-sheet>
+              <v-btn color="warning" class="mt-4" block @click="removeWord"> Remove Word </v-btn>
+            </div>
+          </v-form>
+        </v-sheet>
       </v-dialog>
     </v-btn>
   </div>
@@ -66,6 +38,7 @@
 
 <script lang="ts">
 import { ref } from 'vue'
+import Axios from 'axios'
 
 export default {
   data() {
@@ -74,10 +47,10 @@ export default {
       input: '',
       valid: true,
       wordRules: [
-        (v : string) => !!v || 'Word is required',
-        (v : string) =>  ! /[^\p{L}]/u.test(v) || 'Word must only contain letters',
-        (v : string) => (v && v.length === 5) || 'Word must be 5 characters',
-      ],
+        (v: string) => !!v || 'Word is required',
+        (v: string) => !/[^\p{L}]/u.test(v) || 'Word must only contain letters',
+        (v: string) => (v && v.length === 5) || 'Word must be 5 characters'
+      ]
     }
   },
   methods: {
@@ -85,13 +58,22 @@ export default {
       return this.input.length === 5
     },
     removeWord() {
-      console.log('Remove Word: ' + this.input)
+      // This will have to make the Axios request for removing a word
+      Axios.get(`/Word/RemoveWord?word=${this.input}`).then((response) => {
+        console.log(response.data)
+      })
     },
     addWord() {
-      console.log('Add Word: ' + this.input)
+      // Axios request for adding a word
+      Axios.get(`/Word/AddWord?word=${this.input}`).then((response) => {
+        console.log(response.data)
+      })
     },
     flipIsCommon() {
-      console.log('Flip is common: ' + this.input)
+      // Axios request for flipping if a word is common
+      Axios.get(`/Word/FlipIsCommon?word=${this.input}`).then((response) => {
+        console.log(response.data)
+      })
     }
   }
 }
