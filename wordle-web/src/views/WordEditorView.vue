41be0d42ce49490298d7b254a9903bb1f3f3a551
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2023-06-03 21:07:18 -0700

    WordFlag -changes common flag of word (removed from WordList)

--- wordle-web/src/views/WordEditorView.vue
+++ wordle-web/src/views/WordEditorView.vue
@@ -18,4 +18,5 @@
 <script setup lang="ts">
 import UpdateWordList from '../components/UpdateWordList.vue'
 import WordList from '../components/WordList.vue'
+import WordFlag from '../components/WordFlag.vue';
 </script>
