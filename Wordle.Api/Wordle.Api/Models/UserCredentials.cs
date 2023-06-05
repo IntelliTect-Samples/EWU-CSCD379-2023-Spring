9e7c9b3419feb85e3642a794a8cb309dd054b72e
Hunter T <strangeranger01@randomserver.xyz>
2023-06-05 10:53:34 -0700

    Fix problem with GetToken API

--- Wordle.Api/Wordle.Api/Models/UserCredentials.cs
+++ Wordle.Api/Wordle.Api/Models/UserCredentials.cs
@@ -5,7 +5,12 @@ public class UserCredentials
     public string Username { get; set; }
     public string Password { get; set; }
 
-    protected UserCredentials(string username, string password)
+    /* Must remain public, else the following error is thrown when connecting to 'GetToken' API:
+     System.NotSupportedException: Deserialization of types without a parameterless constructor, a singular
+     parameterized constructor, or a parameterized constructor annotated with 'JsonConstructorAttribute' is not
+     supported. Type 'Wordle.Api.Models.UserCredentials'. Path: $ | LineNumber: 0 | BytePositionInLine: 1.
+     */
+    public UserCredentials(string username, string password)
     {
         Username = username;
         Password = password;
