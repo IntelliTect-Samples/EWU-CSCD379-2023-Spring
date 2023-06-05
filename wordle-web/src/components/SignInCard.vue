d7d17060abd4254ecc5b88496628cab856ca5a8a
Danielson77789 <59825907+Danielson77789@users.noreply.github.com>
2023-06-04 21:16:32 -0700

    Getting login and word edit added

--- wordle-web/src/components/SignInCard.vue
+++ wordle-web/src/components/SignInCard.vue
@@ -1,34 +1,37 @@
 <template>
-    <v-btn @click="showDialog = true">
-        Sign In!
-    </v-btn>
+  <v-btn @click="showDialog = true"> Sign In! </v-btn>
   <v-dialog :model-value="showDialog" @update:model-value="showDialog = !showDialog">
     <v-card>
-        <v-card-title>
-            Sign In
-        </v-card-title>
-        <v-text-field class="pa-5" v-model="username" label="Username" variant="outlined"></v-text-field>
-        <v-text-field class="pa-5" v-model="password" label="Password" variant="outlined"></v-text-field>
-        <v-btn class="pb-5" @click="signIn(username, password)">
-            Sign In
-        </v-btn>
+      <v-card-title> Sign In </v-card-title>
+      <v-text-field
+        class="pa-5"
+        v-model="username"
+        label="Username"
+        variant="outlined"
+      ></v-text-field>
+      <v-text-field
+        class="pa-5"
+        v-model="password"
+        label="Password"
+        variant="outlined"
+      ></v-text-field>
+      <v-btn class="pb-5" @click="signIn(username, password)"> Sign In </v-btn>
     </v-card>
   </v-dialog>
 </template>
-  
+
 <script setup lang="ts">
 import { ref, inject } from 'vue'
 import type { SignInService } from '../scripts/signInService'
 import { Services } from '../scripts/services'
 
-
 const signInService = inject(Services.SignInService) as SignInService
 
-const showDialog = ref(false);
-const username = ref('');
-const password = ref('');
+const showDialog = ref(false)
+const username = ref('')
+const password = ref('')
 
-function signIn(user: string,pass: string) {
-    signInService.signInAsync(user, pass);
+function signIn(user: string, pass: string) {
+  signInService.signInAsync(user, pass)
 }
 </script>
--- wordle-web/src/views/WordEditView.vue
+++ wordle-web/src/views/WordEditView.vue
@@ -1,10 +1,9 @@
 <template>
-    <v-card class="ma-5" elevation="5">
-      <v-card-title>Word Editor</v-card-title>
-      <v-card-text>
-  
-        <v-table density="compact">
-          <v-list density="compact">
+  <v-card class="ma-5" elevation="5">
+    <v-card-title>Word Editor</v-card-title>
+    <v-card-text>
+      <v-table density="compact">
+        <v-list density="compact">
           <v-list-subheader>Word</v-list-subheader>
 
           <v-list-item
@@ -17,123 +16,118 @@
             <template v-slot:prepend>
               <v-icon color="red" @click="deleteWord(list[i].wordId)">mdi-delete</v-icon>
             </template>
-          
-          
+
             <div class="d-flex space-evenly-between flex-row">
               <h2 class="pr-15 pl-5 pt-5" v-text="item.text"></h2>
 
               <v-checkbox v-model="item.isCommon" label="Is Word Common"></v-checkbox>
               <v-checkbox v-model="item.isUsed" label="Is Word Used"></v-checkbox>
             </div>
-        
           </v-list-item>
         </v-list>
-        </v-table>
-        <v-row class="d-flex justify-center">
-          <v-col cols="12" md="4">
-            <v-text-field v-model="options.search" label="Search" />
-          </v-col>
-          <v-col cols="12" md="2">
-            <v-btn @click="addWord" color="grey">
-              <v-icon>mdi-plus</v-icon>
-              Add new word
-            </v-btn>
-          </v-col>
-        </v-row>
-          
-      </v-card-text>
+      </v-table>
+      <v-row class="d-flex justify-center">
+        <v-col cols="12" md="4">
+          <v-text-field v-model="options.search" label="Search" />
+        </v-col>
+        <v-col cols="12" md="2">
+          <v-btn @click="addWord" color="grey">
+            <v-icon>mdi-plus</v-icon>
+            Add new word
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
 
-      
-    </v-card>
-  </template>
-  
-  <script setup lang="ts">
-  import Axios from 'axios'
-  import { inject, reactive } from 'vue'
-  import { WordListOptions, WordListItem } from '@/scripts/wordEdit'
-  import { watch } from 'vue'
-  import type { SignInService } from '@/scripts/signInService'
-  import { Services } from '@/scripts/services'
-  
-  const options = reactive(new WordListOptions())
-  const list = reactive(new Array<WordListItem>())
-  const signInService = inject(Services.SignInService) as SignInService
-  
-  watch(options, () => {
-    refreshList()
-  })
-  
+<script setup lang="ts">
+import Axios from 'axios'
+import { inject, reactive } from 'vue'
+import { WordListOptions, WordListItem } from '@/scripts/wordEdit'
+import { watch } from 'vue'
+import type { SignInService } from '@/scripts/signInService'
+import { Services } from '@/scripts/services'
+
+const options = reactive(new WordListOptions())
+const list = reactive(new Array<WordListItem>())
+const signInService = inject(Services.SignInService) as SignInService
+
+watch(options, () => {
   refreshList()
-  
-  function refreshList() {
-    console.log(options.search);
-    Axios.get('word/GetWordlList', {
-      params: { options }
+})
+
+refreshList()
+
+function refreshList() {
+  console.log(options.search)
+  Axios.get('word/GetWordlList', {
+    params: { options }
+  })
+    .then((response) => {
+      console.log(options)
+      console.log(response)
+      list.length = 0
+      response.data.forEach((item: WordListItem) => {
+        list.push(item)
+      })
     })
-      .then((response) => {
-        console.log(options)
-        console.log(response)
-        list.length = 0
-        response.data.forEach((item: WordListItem) => {
-          list.push(item)
-        })
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+function deleteWord(id: number) {
+  Axios.delete('word', {
+    params: {
+      wordId: id
+    }
+  })
+    .then((response) => {
+      console.log(response.data)
+      refreshList()
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+function updateWord(word: WordListItem) {
+  console.log(word)
+  Axios.post('word/UpdateWord', word)
+    .then((response) => {
+      console.log(response.data)
+      refreshList()
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+function addWord() {
+  // User types in the word in a dialog box
+  const text = prompt('Enter the word to add')
+  if (!text) {
+    return
   }
-  
-  function deleteWord(id: number) {
-    Axios.delete('word', {
-      params: {
-        wordId: id
+  Axios.post('word/AddWordFromBody', {
+    text: text,
+    isCommon: true,
+    isUsed: false
+  })
+    .then((response) => {
+      console.log(response.data)
+      if (response.data.wasSuccessful) {
+        options.search = text
+        options.pageNumber = 1
+        refreshList()
+      } else {
+        alert('Could not add word. ' + response.data.message)
       }
     })
-      .then((response) => {
-        console.log(response.data)
-        refreshList()
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }
-  
-  function updateWord(word: WordListItem) {
-    console.log(word)
-    Axios.post('word/UpdateWord', word)
-      .then((response) => {
-        console.log(response.data)
-        refreshList()
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }
-  
-  function addWord() {
-    // User types in the word in a dialog box
-    const text = prompt('Enter the word to add')
-    if (!text) {
-      return
-    }
-    Axios.post('word/AddWordFromBody', {
-      text: text,
-      isCommon: true,
-      isUsed: false
+    .catch((error) => {
+      console.log(error)
+      alert('Could not add word. An error occurred.')
     })
-      .then((response) => {
-        console.log(response.data)
-        if (response.data.wasSuccessful) {
-          options.search = text
-          options.pageNumber = 1
-          refreshList()
-        } else {
-          alert('Could not add word. ' + response.data.message)
-        }
-      })
-      .catch((error) => {
-        console.log(error)
-        alert('Could not add word. An error occurred.')
-      })
-  }
-  </script>
+}
+</script>
