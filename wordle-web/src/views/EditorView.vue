981df42581aec43c6c177663dd6c390ce84b9866
FenrisStargazer <80987915+FenrisStargazer@users.noreply.github.com>
2023-06-05 15:49:20 -0700

    stuff

--- wordle-web/src/views/EditorView.vue
+++ wordle-web/src/views/EditorView.vue
@@ -2,11 +2,7 @@
   <v-parallax height="650" src="https://source.unsplash.com/zeInZepl_Hw">
     <div class="d-flex flex-column fill-height justify-center align-center text-white">
       <v-card>
-        <v-card-title> Word Editor </v-card-title>
-
-        <div class="text-center">
-          <v-pagination v-model="page" :length="15" :total-visible="6"></v-pagination>
-        </div>
+        <h1>Word Editor</h1>
       </v-card>
     </div>
   </v-parallax>
@@ -14,6 +10,4 @@
 
 <style></style>
 
-<script>
-const page = true
-</script>
+<script></script>
