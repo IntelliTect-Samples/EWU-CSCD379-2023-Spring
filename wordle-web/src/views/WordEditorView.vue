997e28d1012a895007f183af8ae1c4ad4edaa49c
Nolan Posey <nposey1@ewu.edu>
2023-06-05 18:30:36 -0700

    Ran prettier formatting script

--- wordle-web/src/views/WordEditorView.vue
+++ wordle-web/src/views/WordEditorView.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card>
     <v-card-item>
-      <v-table >
+      <v-table>
         <thead>
           <tr>
             <th class="text-center">Word</th>
@@ -12,19 +12,33 @@
           <tr v-for="word in words" :key="word.wordID">
             <td class="text-center">{{ word.text }}</td>
             <td class="text-center">{{ word.isCommon ? 'Yes' : 'No' }}</td>
-            <td><v-btn @click="openEditIsCommon(word.text, word.isCommon)" color="success">Edit Is Common</v-btn></td>
-            <td><v-btn v-if="signInService.token.roles.includes('MasterOfTheUniverse')" @click="openRemoveWord(word.text)" color="error">Remove</v-btn></td>
+            <td>
+              <v-btn @click="openEditIsCommon(word.text, word.isCommon)" color="success"
+                >Edit Is Common</v-btn
+              >
+            </td>
+            <td>
+              <v-btn
+                v-if="signInService.token.roles.includes('MasterOfTheUniverse')"
+                @click="openRemoveWord(word.text)"
+                color="error"
+                >Remove</v-btn
+              >
+            </td>
           </tr>
         </tbody>
       </v-table>
     </v-card-item>
 
     <v-card-item>
-        <v-text-field v-model="searchWord" label="Search" @update:model-value="updateSearch()" />
+      <v-text-field v-model="searchWord" label="Search" @update:model-value="updateSearch()" />
     </v-card-item>
 
-    <v-card-item class="justify-center" v-if="signInService.token.roles.includes('MasterOfTheUniverse')">
-        <v-btn @click="addWordDialog = !addWordDialog" color="primary">Add Word</v-btn>
+    <v-card-item
+      class="justify-center"
+      v-if="signInService.token.roles.includes('MasterOfTheUniverse')"
+    >
+      <v-btn @click="addWordDialog = !addWordDialog" color="primary">Add Word</v-btn>
     </v-card-item>
 
     <v-card-actions>
@@ -35,66 +49,72 @@
     </v-card-actions>
   </v-card>
 
-  <v-dialog v-model="addWordDialog" class="align-center justify-center" max-width="400px" persistent>
-    <v-card >
-        <v-card-item>
-            <v-text-field v-model="addWordText" label="New Word"></v-text-field>
-        </v-card-item>
-
-        <v-card-item>
-            <v-select
-                v-model="addWordIsCommon"
-                label="Is Common"
-                :items="['No', 'Yes']"
-            >
-            </v-select>
-        </v-card-item>
-
-        <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn class="bg-success" @click="addWord()" >Submit</v-btn>
-            <v-btn @click="addWordDialog = !addWordDialog" variant="outlined">Close</v-btn>
-        </v-card-actions>
-    </v-card>
-  </v-dialog>
-
-  <v-dialog v-model="editIsCommonDialog" class="align-center justify-center" max-width="400px" persistent>
-    <v-card >
-        <v-card-item>
-            <v-card-text>
-                Word: {{ editIsCommonWord.text }}   -   Is Common: {{ editIsCommonWord.isCommon ? 'Yes' : 'No' }}
-            </v-card-text>
-        </v-card-item>
-
-        <v-card-item>
-            <v-select
-                v-model="addWordIsCommon"
-                label="Is Common"
-                :items="['No', 'Yes']"
-            >
-            </v-select>
-        </v-card-item>
-
-        <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn class="bg-success" @click="submitEditIsCommon()" >Submit</v-btn>
-            <v-btn @click="editIsCommonDialog = !editIsCommonDialog" variant="outlined">Close</v-btn>
-        </v-card-actions>
-    </v-card>
-  </v-dialog>
-
-  <v-dialog v-model="removeWordDialog" class="align-center justify-center" max-width="400px" persistent>
+  <v-dialog
+    v-model="addWordDialog"
+    class="align-center justify-center"
+    max-width="400px"
+    persistent
+  >
     <v-card>
-        <v-card-item>
-            <v-card-text>Are you sure you want to remove {{ wordToRemove.text }}?</v-card-text>
-        </v-card-item>
+      <v-card-item>
+        <v-text-field v-model="addWordText" label="New Word"></v-text-field>
+      </v-card-item>
 
-        <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn @click="submitRemoveWord()" class="bg-error">Yes, I'm sure</v-btn>
-            <v-btn @click="removeWordDialog = !removeWordDialog" variant="outlined">Cancel</v-btn>
-            <v-spacer></v-spacer>
-        </v-card-actions>
+      <v-card-item>
+        <v-select v-model="addWordIsCommon" label="Is Common" :items="['No', 'Yes']"> </v-select>
+      </v-card-item>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn class="bg-success" @click="addWord()">Submit</v-btn>
+        <v-btn @click="addWordDialog = !addWordDialog" variant="outlined">Close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <v-dialog
+    v-model="editIsCommonDialog"
+    class="align-center justify-center"
+    max-width="400px"
+    persistent
+  >
+    <v-card>
+      <v-card-item>
+        <v-card-text>
+          Word: {{ editIsCommonWord.text }} - Is Common:
+          {{ editIsCommonWord.isCommon ? 'Yes' : 'No' }}
+        </v-card-text>
+      </v-card-item>
+
+      <v-card-item>
+        <v-select v-model="addWordIsCommon" label="Is Common" :items="['No', 'Yes']"> </v-select>
+      </v-card-item>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn class="bg-success" @click="submitEditIsCommon()">Submit</v-btn>
+        <v-btn @click="editIsCommonDialog = !editIsCommonDialog" variant="outlined">Close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <v-dialog
+    v-model="removeWordDialog"
+    class="align-center justify-center"
+    max-width="400px"
+    persistent
+  >
+    <v-card>
+      <v-card-item>
+        <v-card-text>Are you sure you want to remove {{ wordToRemove.text }}?</v-card-text>
+      </v-card-item>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn @click="submitRemoveWord()" class="bg-error">Yes, I'm sure</v-btn>
+        <v-btn @click="removeWordDialog = !removeWordDialog" variant="outlined">Cancel</v-btn>
+        <v-spacer></v-spacer>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
@@ -127,16 +147,16 @@ Axios.get('Word/WordList?pageNumber=0').then((response) => {
 })
 
 function nextPage() {
-    page++
-    if (searchWord.value === '') {
-        Axios.get('Word/WordList?pageNumber=' + page).then((response) => {
+  page++
+  if (searchWord.value === '') {
+    Axios.get('Word/WordList?pageNumber=' + page).then((response) => {
+      words.value = response.data as WordHelper[]
+    })
+  } else {
+    Axios.get('Word/WordList?pageNumber=' + page + '&searchWord=' + searchWord.value).then(
+      (response) => {
         words.value = response.data as WordHelper[]
-        })
-    } else {
-        Axios.get('Word/WordList?pageNumber=' + page + '&searchWord=' + searchWord.value).then(
-        (response) => {
-            words.value = response.data as WordHelper[]
-        }
+      }
     )
   }
 }
@@ -169,83 +189,86 @@ function updateSearch() {
 }
 
 function addWord() {
-    let text = addWordText.value.toLowerCase()
-    let common = false
+  let text = addWordText.value.toLowerCase()
+  let common = false
 
-    if(addWordIsCommon.value === 'Yes') {
-        common = true
-    }
+  if (addWordIsCommon.value === 'Yes') {
+    common = true
+  }
 
-    if(text.length === 5) {
-        const newWord = new WordHelper()
-        newWord.text = text
-        newWord.isCommon = common
-        Axios.post('/Word/AddWord', newWord).then( () => {
-            addWordText.value = ''
-            addWordIsCommon.value = ''
-            if (searchWord.value === '') {
-                Axios.get('Word/WordList?pageNumber=' + page).then((response) => {
-                    words.value = response.data as WordHelper[]
-                })
-            } else {
-                Axios.get('Word/WordList?pageNumber=' + page + '&searchWord=' + searchWord.value).then(
-                    (response) => {
-                        words.value = response.data as WordHelper[]
-                    })
-            }
+  if (text.length === 5) {
+    const newWord = new WordHelper()
+    newWord.text = text
+    newWord.isCommon = common
+    Axios.post('/Word/AddWord', newWord).then(() => {
+      addWordText.value = ''
+      addWordIsCommon.value = ''
+      if (searchWord.value === '') {
+        Axios.get('Word/WordList?pageNumber=' + page).then((response) => {
+          words.value = response.data as WordHelper[]
         })
-    }
-    addWordDialog.value = false
+      } else {
+        Axios.get('Word/WordList?pageNumber=' + page + '&searchWord=' + searchWord.value).then(
+          (response) => {
+            words.value = response.data as WordHelper[]
+          }
+        )
+      }
+    })
+  }
+  addWordDialog.value = false
 }
 
 function openRemoveWord(wordText: string) {
-    wordToRemove.value.text = wordText
-    removeWordDialog.value = !removeWordDialog.value
+  wordToRemove.value.text = wordText
+  removeWordDialog.value = !removeWordDialog.value
 }
 
-function submitRemoveWord(){
-    Axios.post('/Word/RemoveWord', wordToRemove.value).then( () => {
-        removeWordDialog.value = !removeWordDialog.value
-        if (searchWord.value === '') {
-        Axios.get('Word/WordList?pageNumber=' + page).then((response) => {
-            words.value = response.data as WordHelper[]
-        })
-        } else {
-            Axios.get('Word/WordList?pageNumber=' + page + '&searchWord=' + searchWord.value).then(
-                (response) => {
-                    words.value = response.data as WordHelper[]
-                })
+function submitRemoveWord() {
+  Axios.post('/Word/RemoveWord', wordToRemove.value).then(() => {
+    removeWordDialog.value = !removeWordDialog.value
+    if (searchWord.value === '') {
+      Axios.get('Word/WordList?pageNumber=' + page).then((response) => {
+        words.value = response.data as WordHelper[]
+      })
+    } else {
+      Axios.get('Word/WordList?pageNumber=' + page + '&searchWord=' + searchWord.value).then(
+        (response) => {
+          words.value = response.data as WordHelper[]
         }
-    })
+      )
+    }
+  })
 }
 
 function openEditIsCommon(wordText: string, isCommon: boolean) {
-    editIsCommonWord.value.text = wordText
-    editIsCommonWord.value.isCommon = isCommon
-    editIsCommonDialog.value = !editIsCommonDialog.value
+  editIsCommonWord.value.text = wordText
+  editIsCommonWord.value.isCommon = isCommon
+  editIsCommonDialog.value = !editIsCommonDialog.value
 }
 
 function submitEditIsCommon() {
-    if(addWordIsCommon.value === 'Yes') {
-        editIsCommonWord.value.isCommon = true
-    } else {
-        editIsCommonWord.value.isCommon = false
-    }
+  if (addWordIsCommon.value === 'Yes') {
+    editIsCommonWord.value.isCommon = true
+  } else {
+    editIsCommonWord.value.isCommon = false
+  }
 
-    Axios.post('/Word/AddWord', editIsCommonWord.value).then( () => {
-        editIsCommonWord.value.text = ''
-        editIsCommonWord.value.isCommon = false
-        editIsCommonDialog.value = !editIsCommonDialog.value
-        if (searchWord.value === '') {
-        Axios.get('Word/WordList?pageNumber=' + page).then((response) => {
-            words.value = response.data as WordHelper[]
-        })
-        } else {
-            Axios.get('Word/WordList?pageNumber=' + page + '&searchWord=' + searchWord.value).then(
-                (response) => {
-                    words.value = response.data as WordHelper[]
-                })
+  Axios.post('/Word/AddWord', editIsCommonWord.value).then(() => {
+    editIsCommonWord.value.text = ''
+    editIsCommonWord.value.isCommon = false
+    editIsCommonDialog.value = !editIsCommonDialog.value
+    if (searchWord.value === '') {
+      Axios.get('Word/WordList?pageNumber=' + page).then((response) => {
+        words.value = response.data as WordHelper[]
+      })
+    } else {
+      Axios.get('Word/WordList?pageNumber=' + page + '&searchWord=' + searchWord.value).then(
+        (response) => {
+          words.value = response.data as WordHelper[]
         }
-    })
+      )
+    }
+  })
 }
 </script>
