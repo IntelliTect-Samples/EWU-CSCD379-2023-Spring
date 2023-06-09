dda9bd8daeb03407b5e6b61ae44d4238416c465e
VForev <85714161+VForev@users.noreply.github.com>
2023-06-08 19:28:25 -0700

    Nick db connections

--- wordle-web/src/main.ts
+++ wordle-web/src/main.ts
@@ -20,7 +20,7 @@ import { PlayerService } from './scripts/playerService'
 if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
   Axios.defaults.baseURL = 'https://localhost:7053/'
 } else {
-  Axios.defaults.baseURL = 'https://wordle2023dillon.azurewebsites.net/'
+  Axios.defaults.baseURL = 'https://cscd-379-spring-2023-nickb.azurewebsites.net/'
 }
 
 // Provide the useDisplay to other components so that it can be used in testing.
