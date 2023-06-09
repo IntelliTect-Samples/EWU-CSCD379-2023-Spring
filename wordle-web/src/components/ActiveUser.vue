2c6848a717329f55c11859f781aee18ad09b1bce
AndrewKorchemniy <andrewkorch@gmail.com>
2023-06-08 22:41:47 -0700

    Add login dialog

--- wordle-web/src/components/ActiveUser.vue
+++ wordle-web/src/components/ActiveUser.vue
@@ -1,49 +1,76 @@
 <template>
   <v-btn @click="showDialog = true">
-    {{ playerService.player.name }}
+    <span v-if="!signInService.isSignedIn">Not signed in</span>
+    <span v-else>{{ signInService.token.userName }}</span>
   </v-btn>
-  <v-dialog :model-value="showDialog" @update:model-value="close" persistent>
-    <v-card>
-      <v-card-text>
-        Set or change your username:
-        <v-text-field ref="$input" v-model="newName" @keyup.enter="confirm" />
+  <v-dialog :model-value="showDialog" @update:model-value="close">
+    <v-card
+      :class="display.xs ? 'w-100' : display.sm ? 'w-75' : display.md ? 'w-50' : 'w-25'"
+      class="justify-center mx-auto"
+    >
+      <v-card-text v-if="!signInService.isSignedIn">
+        <v-text-field
+          ref="$input"
+          v-model="username"
+          label="User Name"
+          outlined
+          dense
+          hide-details
+          class="mt-3"
+        ></v-text-field>
+        <v-text-field
+          v-model="password"
+          label="Password"
+          type="password"
+          outlined
+          dense
+          hide-details
+          class="mt-3 mb-0"
+        ></v-text-field>
       </v-card-text>
-      <v-card-actions>
-        <v-spacer> </v-spacer>
-        <v-btn @click="confirm">OK</v-btn>
-        <v-btn @click="close">Cancel</v-btn>
+      <v-card-actions class="ml-3 mb-3">
+        <v-btn @click="signOut" v-if="signInService.isSignedIn" class="mt-3">Sign Out</v-btn>
+        <v-btn @click="signIn" v-else>Sign In</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script lang="ts" setup>
-import type { PlayerService } from '@/scripts/playerService'
 import { Services } from '@/scripts/services'
-import { nextTick } from 'vue'
-import { watch } from 'vue'
+import type { SignInService } from '@/scripts/signInService'
+import { reactive } from 'vue'
 import { inject } from 'vue'
 import { ref } from 'vue'
+import { useDisplay } from 'vuetify/lib/framework.mjs'
 
-const playerService = inject(Services.PlayerService) as PlayerService
-let newName = playerService.player.name
+const signInService = inject(Services.SignInService) as SignInService
+
+const display = inject(Services.Display, () => reactive(useDisplay())) as unknown as ReturnType<
+  typeof useDisplay
+>
+
+// refs for storing the username and password
+const username = ref('')
+const password = ref('')
 
 const showDialog = ref(false)
 const $input = ref<HTMLInputElement>()
 
-watch(showDialog, (value) => {
-  if (value) {
-    nextTick(() => {
-      $input.value!.focus()
-      $input.value!.select()
-    })
+const signIn = async () => {
+  await signInService.signInAsync(username.value, password.value)
+  if (signInService.isSignedIn) {
+    close()
+  } else {
+    $input.value?.focus()
   }
-})
+}
 
-const confirm = async () => {
-  await playerService.ChangeNameAsync(newName)
+const signOut = () => {
+  signInService.signOut()
   close()
 }
+
 const close = () => {
   showDialog.value = false
 }
