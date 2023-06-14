a2de796ca8f08a1bbab3067b65ef228d014ce294
Clockknight <hello@tylerhanyinwang.com>
2023-06-14 01:59:45 -0700

    Skeleton game page

--- WIK-web/src/components/LoginPopout.vue
+++ WIK-web/src/components/LoginPopout.vue
@@ -18,8 +18,18 @@
 
         <v-chip-group outlined class="justify-center ma-10 pa-10">
           <v-card-title class="text-center">Please enter your information:</v-card-title>
-          <v-text-field :disabled="logout" label="User" v-model="username" variant="outlined"></v-text-field>
-          <v-text-field :disabled="logout" label="Password" v-model="password" variant="outlined"></v-text-field>
+          <v-text-field
+            :disabled="logout"
+            label="User"
+            v-model="username"
+            variant="outlined"
+          ></v-text-field>
+          <v-text-field
+            :disabled="logout"
+            label="Password"
+            v-model="password"
+            variant="outlined"
+          ></v-text-field>
           <v-card-title>{{ errorText }}</v-card-title>
         </v-chip-group>
         <v-chip-group class="justify-center ma-10 pa-10">
@@ -33,7 +43,7 @@
 </template>
 
 <script setup lang="ts">
-import  Axios  from 'axios'
+import Axios from 'axios'
 import { ref } from 'vue'
 
 const username = ref('')
@@ -45,61 +55,57 @@ const login = ref(localStorage.getItem('username') === null)
 const logout = ref(localStorage.getItem('username') !== null)
 
 function wordleLogin() {
-
-  Axios.get("/User?username=" + username.value + "&password="+ password.value)
+  Axios.get('/User?username=' + username.value + '&password=' + password.value)
     .then((response) => {
       console.log(response.data)
-      if(response.data !== "Invalid Username or Password"){
+      if (response.data !== 'Invalid Username or Password') {
         setLoginInfo()
         open.value = false
         login.value = false
         logout.value = true
         errorText.value = ''
         window.location.reload()
-      }
-      else{
-        errorText.value = "Invalid Username or Password"
+      } else {
+        errorText.value = 'Invalid Username or Password'
       }
     })
     .catch((error) => {
       console.log(error)
     })
-    
 }
 
-function setLoginInfo(){
+function setLoginInfo() {
   localStorage.setItem('username', username.value)
   localStorage.setItem('password', password.value)
   username.value = ''
   password.value = ''
 }
 
-function logoutUser(){
+function logoutUser() {
   localStorage.removeItem('username')
   localStorage.removeItem('password')
   login.value = true
   logout.value = false
   window.location.reload()
 }
-  
 
-  // if (username.value == 'Realuser') {
-  //   result.value = 'Wrong password. Try "im20dude".'
-  //   if (password.value == 'im20dude') {
-  //     localStorage.setItem('user', username.value)
-  //     localStorage.setItem('age', 'under')
-  //     result.value = 'Success. use "im21dude" if you are normal and can be trusted.'
-  //   } else if (password.value == 'im21dude') {
-  //     localStorage.setItem('user', username.value)
-  //     localStorage.setItem('age', 'above')
-  //     result.value = 'Success. You can update common words.'
-  //   }
-  // } else {
-  //   result.value = 'Wrong user. Try "Realuser".'
-  // }
+// if (username.value == 'Realuser') {
+//   result.value = 'Wrong password. Try "im20dude".'
+//   if (password.value == 'im20dude') {
+//     localStorage.setItem('user', username.value)
+//     localStorage.setItem('age', 'under')
+//     result.value = 'Success. use "im21dude" if you are normal and can be trusted.'
+//   } else if (password.value == 'im21dude') {
+//     localStorage.setItem('user', username.value)
+//     localStorage.setItem('age', 'above')
+//     result.value = 'Success. You can update common words.'
+//   }
+// } else {
+//   result.value = 'Wrong user. Try "Realuser".'
+// }
 
-  // console.log(localStorage.getItem('user'))
-  // console.log(localStorage.getItem('age'))
+// console.log(localStorage.getItem('user'))
+// console.log(localStorage.getItem('age'))
 
 let dialog = ref(false)
 </script>
--- WIK-web/src/views/GameView.vue.vue
+++ WIK-web/src/views/GameView.vue.vue
@@ -1,7 +1,33 @@
 <template>
-  <v-row class="justify-center"
-    ><h1>{{ titleRef }}</h1></v-row
-  >
+  <v-col class="px-16 py-10 my-10" align="center">
+    <v-container class="px-4 py-10 my-10" style="max-width: 1000px">
+      <!--testing to try and get any to work-->
+      <video-embed src="https://www.youtube.com/watch?v=s4ObxcdXoFE"></video-embed>
+      <video-embed src="https://vimeo.com/370293725"></video-embed>
+      <video-embed src="https://dai.ly/x7n7y06"></video-embed>
+      <video-embed src="https://coub.com/embed/22eztb"></video-embed>
+      <!-- 
+        Actual WIK clips (shockingly small amount)
+        https://twitter.com/EVO/status/1419024349692973061
+        https://www.youtube.com/watch?v=DEGjCWxMnm4
+        https://www.youtube.com/watch?v=ko-6nHuBf8g
+        https://www.youtube.com/watch?v=ti3NGlVO7Bs
+      -->
+    </v-container>
+
+    <v-row class="px-16" style="max-width: 600px">
+      <v-btn @click="guessSide(-1)" class="my-8">leftist</v-btn>
+      <v-col>
+        <h3>{{ points }}</h3>
+        <v-text-field class="px-8"></v-text-field>
+      </v-col>
+      <v-btn @click="guessSide(1)" class="my-8">rightist</v-btn>
+    </v-row>
+    <v-row class="center px-16" style="max-width: 300px">
+      <v-btn @click="submit">Test test test test</v-btn>
+    </v-row>
+  </v-col>
+  <!--
   <v-container>
     <GameBoard :game="game" @letterClick="addChar" />
   </v-container>
@@ -24,6 +50,7 @@
 
   <v-btn @click="testApi">API</v-btn>
   <NameCardDialog :playerName="game.currentPlayer" @enterChar="setName"></NameCardDialog>
+  -->
 </template>
 
 <script setup lang="ts">
@@ -37,15 +64,40 @@ import NameCardDialog from '@/components/NameCardDialog.vue'
 import { useRoute } from 'vue-router'
 import Axios from 'axios'
 import { WordsService } from '@/scripts/wordsService'
+import Embed from 'v-video-embed'
 
-const titleRef = ref('Wordle')
+const titleRef = ref('')
 const guess = ref('')
 const game = reactive(new WordleGame())
 const route = useRoute()
 const timer = ref(0)
+let sideGuess = 0
+let points = ref(0)
 let tracker = setInterval(myTimer, 1000)
 
-console.log(game.secretWord)
+function myTimer() {
+  timer.value = timer.value + 1
+}
+function autoComplete(fill: string) {
+  while (game.guess.text !== '') {
+    game.guess.pop()
+  }
+  for (let i = 0; i < fill.length; i++) {
+    game.guess.push(fill.charAt(i))
+  }
+}
+
+function guessSide(side: number) {
+  sideGuess = side
+  //enable submit button
+}
+function submit() {
+  // if guessSide == correctSide
+  // points += betValue
+  // else
+  // points -= betValue
+}
+
 onMounted(async () => {
   window.addEventListener('keyup', keyPress)
   await newGame()
@@ -54,17 +106,16 @@ onMounted(async () => {
 onUnmounted(() => {
   window.removeEventListener('keyup', keyPress)
 })
-
 function newGame() {
   let apiPath = 'word'
   if (route.path == '/wordoftheday') {
     apiPath = `word/wordoftheday?offsetInHours=${new Date().getTimezoneOffset() / -60}`
-    titleRef.value = 'Word Of The Day'
+    titleRef.value = 'Clip Of The Day'
     if (route.query.date) {
       apiPath += `&date=${route.query.date}`
     }
   } else {
-    titleRef.value = 'Wordle'
+    titleRef.value = 'Will It Kill?'
   }
   Axios.get(apiPath)
     .then((response) => {
@@ -79,29 +130,6 @@ function newGame() {
   clearInterval(tracker)
   tracker = setInterval(myTimer, 1000)
 }
-
-function myTimer() {
-  timer.value = timer.value + 1
-}
-function autoComplete(fill: string) {
-  while (game.guess.text !== '') {
-    game.guess.pop()
-  }
-  for (let i = 0; i < fill.length; i++) {
-    game.guess.push(fill.charAt(i))
-  }
-}
-function checkGuess() {
-  if (game.submitGuess(route.path, timer.value)) {
-    console.log(timer.value)
-    clearInterval(tracker)
-  }
-  guess.value = ''
-}
-function addChar(letter: Letter) {
-  game.guess.push(letter.char)
-  guess.value += letter.char
-}
 function keyPress(event: KeyboardEvent) {
   console.log(event.key)
   if (event.key === 'Enter') {
@@ -115,11 +143,23 @@ function keyPress(event: KeyboardEvent) {
     game.guess.push(event.key.toLowerCase())
   }
 }
+function checkGuess() {
+  if (game.submitGuess(route.path, timer.value)) {
+    console.log(timer.value)
+    clearInterval(tracker)
+  }
+  guess.value = ''
+}
+/*
+function addChar(letter: Letter) {
+  game.guess.push(letter.char)
+  guess.value += letter.char
+}
 
 function testApi() {
   game.postPlayerToApi('Dan', 4, 20)
 }
 function setName(name: string) {
   game.setPlayerName(name)
-}
+}*/
 </script>
