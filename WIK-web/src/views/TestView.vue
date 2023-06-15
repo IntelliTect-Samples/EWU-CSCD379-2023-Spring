82f8607729c3b28877eecd26327c82e2680d2a02
Clockknight <hello@tylerhanyinwang.com>
2023-06-14 19:10:52 -0700

    All client side working as intended

--- WIK-web/src/views/TestView.vue
+++ WIK-web/src/views/TestView.vue
@@ -2,24 +2,20 @@
   <v-col class="px-16 py-10 my-10" align="center">
     <v-row>
       <v-col id="clipIdText">Clip ID: {{ clipId }} </v-col>
-      <v-col id="pointText">Points: {{ points }} </v-col>
+      <v-col id="streakText">Streak: {{ streak }} </v-col>
     </v-row>
     <v-container id="element" class="px-4 py-10 my-10" style="max-width: 1000px">
       <iframe id="player" width="100%" height="500" src=""> </iframe>
 
-      <v-row class="px-16" style="max-width: 600px">
-        <v-btn v-show="!showNext" @click="submit(-1)" class="my-8">NO It Won't Kill</v-btn>
-        <v-col>
-          <v-text-field class="px-8"></v-text-field>
-        </v-col>
-        <v-btn v-show="!showNext" @click="submit(1)" class="my-8">YES It Will Kill</v-btn>
+      <v-row class="py-4 my-8" style="max-width: 600px">
+        <v-btn v-show="!showNext" @click="submit(-1)" class="mx-4 px-16">NO It Won't Kill</v-btn>
+        <v-btn v-show="!showNext" @click="submit(1)" class="mx-4 px-16">YES It Will Kill</v-btn>
+        <h2 v-show="showNext">{{ result }}</h2>
       </v-row>
 
-      <v-row class="center px-16" style="max-width: 300px">
+      <v-row class="center px-16 py-4" style="max-width: 300px">
         <v-btn id="nextButton" v-show="showNext" @click="nextClip">Next Next Next Next</v-btn>
       </v-row>
-
-      <!--<v-text-field label="Example"></v-text-field>-->
     </v-container>
   </v-col>
 </template>
@@ -31,22 +27,18 @@ import { useRoute } from 'vue-router'
 import Axios from 'axios'
 import { WordsService } from '@/scripts/wordsService'
 
-const titleRef = ref('')
+const clipId = ref('')
 const guess = ref('')
 const game = reactive(new WordleGame())
 const route = useRoute()
 const timer = ref(0)
-let points = 100
-let tracker = setInterval(myTimer, 1000)
-let clipId = ''
+let showNext = ref(false)
+let streak = ref(0)
 let clipUrl = ''
 let clipStart = ''
 let clipEnd = ''
 let willItKill = ''
-let clip = ref(clipUrl)
-let showNext = false
-
-var nextBtn = document.getElementById('nextButton')
+let result = ref('')
 
 onMounted(async () => {
   nextClip()
@@ -62,13 +54,12 @@ function nextClip() {
     .catch((error) => {
       console.log(error.data)
     })
-  console.log('next no show')
-  showNext = false
+  showNext.value = false
 }
 
 function getClipInfo(response: string) {
   var data = response.split(',')
-  clipId = data[0]
+  clipId.value = data[0]
   clipUrl = data[1]
   clipStart = data[2]
   clipEnd = data[3]
@@ -87,45 +78,31 @@ function getClipInfo(response: string) {
 }
 
 function startGame() {
-  var clipIdText = document.getElementById('clipIdText')
-  var pointText = document.getElementById('pointText')
   var video = document.querySelector('iframe')
 
-  clipIdText?.setAttribute('value', 'Clip ID: Success!')
-  pointText?.setAttribute('value', 'Points: Success!')
   video?.setAttribute('src', clipUrl)
 
-  console.log('start noshow')
-  showNext = false
+  showNext.value = false
 }
 
-function myTimer() {
-  timer.value = timer.value + 1
-}
 function submit(side: number) {
   var video = document.querySelector('iframe')
-  //disable all buttons
-  let betValue = points
-
-  showNext = true
-  console.log('submit show')
-  console.log(showNext)
+  showNext.value = true
   video?.contentWindow?.postMessage('{"event":"command", "func":"playVideo", "args":""}', '*')
 
-  if (betValue > points) {
-    betValue = points
-  }
-
   if ((willItKill == 'True' && side > 0) || (willItKill == 'False' && side < 0)) {
-    points += betValue
+    streak.value += 1
+    console.log(streak)
   } else {
-    points -= betValue
-    if (points <= 0) {
-      points = 100
-    }
+    streak.value = 0
   }
 
   ///User/UpdateUser?username=gamer&win=true
   //you can get the username with localStorage.getItem('username'')
+}
+
+let tracker = setInterval(myTimer, 1000)
+function myTimer() {
+  timer.value = timer.value + 1
 }
 </script>
